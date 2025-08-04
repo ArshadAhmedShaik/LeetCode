142486f847b5dc419d96bc13ef2f31d3c31e4bf9
Shaik Arshad Ahmed <shaik.arshad.dev@gmail.com>
2025-08-04 22:40:04 +0530

    Time: 13 ms (7.11%), Space: 45.1 MB (5.69%) - LeetHub

--- 1741-sort-array-by-increasing-frequency/1741-sort-array-by-increasing-frequency.java
+++ 1741-sort-array-by-increasing-frequency/1741-sort-array-by-increasing-frequency.java
@@ -1,29 +1,17 @@
-class Solution {
-
+public class Solution {
     public int[] frequencySort(int[] nums) {
-
-        Map<Integer, Integer> map = new HashMap<>();
-        for(var x: nums) {
-            map.put(x, map.getOrDefault(x, 0)+1);
+        Map<Integer, Integer> count = new HashMap<>();
+        for (int num : nums) {
+            count.put(num, count.getOrDefault(num, 0) + 1);
         }
 
         Integer[] arr = Arrays.stream(nums).boxed().toArray(Integer[]::new);
+        Arrays.sort(arr, (a, b) -> {
+            int freqA = count.get(a), freqB = count.get(b);
+            if (freqA != freqB) return Integer.compare(freqA, freqB);
+            return Integer.compare(b, a);
+        });
 
-        Arrays.sort(arr, (a,b) -> {
-            int afreq = map.get(a);
-            int bfreq = map.get(b);
-           if(afreq==bfreq) {
-                return b - a;
-           } else {
-            return afreq - bfreq;
-           }
-        }); 
-
-       for (int i = 0; i < nums.length; i++) {
-            nums[i] = arr[i];
-        }
-
-        return nums;
-
+        return Arrays.stream(arr).mapToInt(i -> i).toArray();
     }
 }
