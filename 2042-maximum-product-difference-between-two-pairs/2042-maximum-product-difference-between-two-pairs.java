ccd8abb3cf21d800daacc243da9b641ec91e53ee
Shaik Arshad Ahmed <shaik.arshad.dev@gmail.com>
2025-08-04 10:56:24 +0530

    Time: 6 ms (74.59%), Space: 44.8 MB (95.15%) - LeetHub

--- 2042-maximum-product-difference-between-two-pairs/2042-maximum-product-difference-between-two-pairs.java
+++ 2042-maximum-product-difference-between-two-pairs/2042-maximum-product-difference-between-two-pairs.java
@@ -1,13 +1,10 @@
 class Solution {
-    private int productDiff(int a, int b, int c, int d) {
-                return (a * b) - (c * d);
-    }
+    
     public int maxProductDifference(int[] nums) {
+
         Arrays.sort(nums);
-        int a = nums[nums.length-1];
-        int b = nums[nums.length-2];
-        int c = nums[0];
-        int d = nums[1];
-        return productDiff(a,b,c,d);
+        int n = nums.length;
+        return (nums[n - 1] * nums[n - 2]) - (nums[0] * nums[1]);
     }
+
 }
