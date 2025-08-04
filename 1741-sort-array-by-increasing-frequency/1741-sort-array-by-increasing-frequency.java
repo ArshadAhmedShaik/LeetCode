296cedc60a56169b66f42b9009df9115b3f8301a
Shaik Arshad Ahmed <shaik.arshad.dev@gmail.com>
2025-08-04 22:28:51 +0530

    Time: 4 ms (96.42%), Space: 44.5 MB (78.87%) - LeetHub

--- 1741-sort-array-by-increasing-frequency/1741-sort-array-by-increasing-frequency.java
+++ 1741-sort-array-by-increasing-frequency/1741-sort-array-by-increasing-frequency.java
@@ -1,8 +1,9 @@
 class Solution {
-   
+
     class Pair {
         int num;
         int freq;
+
         Pair(int num, int freq) {
             this.num = num;
             this.freq = freq;
@@ -10,34 +11,28 @@ class Solution {
     }
 
     public int[] frequencySort(int[] nums) {
-       
+
         int[] count = new int[201];
-        for(var x: nums) count[x+100]++;
+        for (var x : nums)
+            count[x + 100]++;
 
         List<Pair> freqList = new ArrayList<>();
 
-        for(int i = 0;i < count.length;i++) {
-            if(count[i]>0) freqList.add(new Pair(i-100, count[i]));
+        for (int i = 0; i < count.length; i++) {
+            if (count[i] > 0)
+                freqList.add(new Pair(i - 100, count[i]));
         }
 
-        Collections.sort(freqList, new Comparator<Pair>() {
-                public int compare(Pair a, Pair b) {
-                        if(a.freq == b.freq) {
-                            return b.num-a.num;
-                        } else {
-                            return a.freq - b.freq;
-                        }
-                }
-        });
+        freqList.sort((a, b) -> (a.freq == b.freq) ? b.num - a.num : a.freq - b.freq);
 
         int[] result = new int[nums.length];
 
         int index = 0;
-        for(Pair p: freqList) {
-                while(p.freq!=0) {
+        for (Pair p : freqList) {
+            while (p.freq != 0) {
                 result[index++] = p.num;
                 p.freq--;
-                }
+            }
         }
 
         return result;
