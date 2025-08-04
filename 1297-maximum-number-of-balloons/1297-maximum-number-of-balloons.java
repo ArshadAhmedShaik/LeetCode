1bc06357af83efc135ad79d4e6e586b181a8ebc3
Shaik Arshad Ahmed <shaik.arshad.dev@gmail.com>
2025-08-05 00:36:50 +0530

    Time: 2 ms (87.48%), Space: 42.1 MB (74.37%) - LeetHub

---
+++ 1297-maximum-number-of-balloons/1297-maximum-number-of-balloons.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int maxNumberOfBalloons(String text) {
+        int[] count = new int[26];
+        for(var x: text.toCharArray()) {
+                count[x-'a']++;
+        }
+     
+     /*bottle-neck question*/
+
+        int b = count[1];
+        int a = count[0];
+        int l = count['l'-'a']/2;
+        int o = count['o'-'a']/2;
+        int n = count['n'-'a'];
+
+        return Math.min(Math.min(Math.min(b, a),Math.min(l,o)),n);
+
+
+    }
+}
