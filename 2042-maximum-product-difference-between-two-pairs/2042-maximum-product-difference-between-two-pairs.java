bd3bf9795503498c125f060761b7904739a84285
Shaik Arshad Ahmed <shaik.arshad.dev@gmail.com>
2025-08-04 11:00:37 +0530

    Time: 4 ms (76.08%), Space: 45.6 MB (11.26%) - LeetHub

--- 2042-maximum-product-difference-between-two-pairs/2042-maximum-product-difference-between-two-pairs.java
+++ 2042-maximum-product-difference-between-two-pairs/2042-maximum-product-difference-between-two-pairs.java
@@ -1,8 +1,22 @@
 class Solution {
-    
+
     public int maxProductDifference(int[] nums) {
 
-        Arrays.sort(nums);
+        int[] count = new int[10001];
+
+        for(var x: nums) {
+            count[x]++;
+        }
+
+        
+        int index = 0;
+        for(int i = 0;i < count.length;i++) {
+                while(count[i]!=0) {
+                        nums[index++] = i;
+                        count[i]--;
+                }
+        }
+        
         int n = nums.length;
         return (nums[n - 1] * nums[n - 2]) - (nums[0] * nums[1]);
     }
