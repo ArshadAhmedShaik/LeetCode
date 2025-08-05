5e14aee4e550626b599011bb36bf2abab744d81a
Shaik Arshad Ahmed <shaik.arshad.dev@gmail.com>
2025-08-05 10:04:25 +0530

    Time: 4 ms (50.28%), Space: 53.9 MB (8.36%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,9 +1,9 @@
 class Solution {
 
-     static{
-        for(int i=0;i<500;i++)
-         majorityElement(new int[]{0});
-    }
+    //  static{
+    //     for(int i=0;i<500;i++)
+    //      majorityElement(new int[]{0});
+    // }
 
     public static int majorityElement(int[] nums) {
 
