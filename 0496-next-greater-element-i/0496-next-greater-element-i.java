9962c8a476002d91610787d3d9ff49acb9be95c5
Shaik Arshad Ahmed <shaik.arshad.dev@gmail.com>
2025-08-05 22:03:27 +0530

    Time: 4 ms (35.96%), Space: 44.3 MB (33.82%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -1,21 +1,22 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
         Map<Integer, Integer> map = new HashMap<>();
-        for(int i = 0;i < nums1.length;i++) {
-                map.put(nums1[i], i);
+        for (int i = 0; i < nums1.length; i++) {
+            map.put(nums1[i], i);
         }
+        Stack<Integer> stack = new Stack<>();
         int[] result = new int[nums1.length];
-        for(int i = 0;i < nums2.length;i++) {
-                if(map.containsKey(nums2[i])) {
-                        int nextGreat = -1;
-                        for(int j = i + 1;j < nums2.length;j++) {
-                                if(nums2[j]>nums2[i]) {
-                                    nextGreat = nums2[j];
-                                    break;
-                                }
-                        }
-                        result[map.get(nums2[i])] = nextGreat;
-                }
+        Arrays.fill(result, -1);
+
+        for (int i = 0; i < nums2.length; i++) {
+               while(!stack.isEmpty() && nums2[i] > stack.peek()) {
+                    int element = stack.pop();
+                    int index = map.get(element);
+                    result[index] = nums2[i];
+               }
+               if(map.containsKey(nums2[i])) {
+                     stack.add(nums2[i]);
+               }
         }
         return result;
     }
