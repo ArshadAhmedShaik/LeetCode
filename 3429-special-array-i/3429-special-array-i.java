e43277b357d2717f555b8d4b6549c043fb1d1435
Shaik Arshad Ahmed <shaik.arshad.dev@gmail.com>
2025-08-05 22:46:13 +0530

    Time: 0 ms (100%), Space: 43.4 MB (43.92%) - LeetHub

---
+++ 3429-special-array-i/3429-special-array-i.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean isArraySpecial(int[] nums) {
+        int n = nums.length;
+        for(int i = 0;i <= n - 2;i++) {
+                if((nums[i]+nums[i+1])%2==0) {
+                        return false;
+                }
+        }
+        return true;
+    }
+}
