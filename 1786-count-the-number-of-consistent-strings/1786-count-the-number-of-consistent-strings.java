1bfcb07f3b5ce11ce1be31a7731b6aa213376373
Shaik Arshad Ahmed <shaik.arshad.dev@gmail.com>
2025-08-07 20:27:55 +0530

    Time: 6 ms (91.07%), Space: 44.9 MB (99.38%) - LeetHub

--- 1786-count-the-number-of-consistent-strings/1786-count-the-number-of-consistent-strings.java
+++ 1786-count-the-number-of-consistent-strings/1786-count-the-number-of-consistent-strings.java
@@ -2,24 +2,25 @@ class Solution {
     public int countConsistentStrings(String allowed, String[] words) {
         int bitMask = 0;
         for(var x: allowed.toCharArray()) {
+                // set the particular bit to one (OR)
                 int temp = 1;
                 int shift = x - 'a';
                 temp = temp<<shift;
                 bitMask = bitMask|temp;
         }
         int count = 0;
-        for(var x: words) {
+        for(var str: words) {
             boolean isConsistent = true;
-            for(var ch: x.toCharArray()) {
-                    int temp = 1;
-                    int shift = ch - 'a';
-                    temp = temp<<shift;
-                    if((temp&bitMask)==0) {
-                        isConsistent = false;
-                        break;
-                    }  
-            }
-            if(isConsistent) count++;
+                for(var ch: str.toCharArray()) {
+                        int temp = 1;
+                        int shift = ch - 'a';
+                        temp = temp<<shift;
+                        if((temp&bitMask)==0) {
+                            isConsistent = false;
+                            break;
+                        } 
+                }
+                if(isConsistent) count++;
         }
         return count;
     }
