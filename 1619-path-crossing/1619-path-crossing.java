e604ad2aef3ce03dec0cf15857cb1df5ec02473c
Shaik Arshad Ahmed <shaik.arshad.dev@gmail.com>
2025-08-08 14:29:33 +0530

    Time: 1 ms (99.6%), Space: 42.5 MB (34.85%) - LeetHub

--- 1619-path-crossing/1619-path-crossing.java
+++ 1619-path-crossing/1619-path-crossing.java
@@ -14,7 +14,7 @@ class Solution {
                 } else {
                         y1--;
                 }
-                int str = x1 * 100000 + y1;
+                int str = x1 * 10000 + y1;
                 if(set.contains(str)) {
                         return true;
                 } else {
