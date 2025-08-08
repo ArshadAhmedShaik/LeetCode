c341a370d2d37527fb7cbe455cd5599ae162c9df
Shaik Arshad Ahmed <shaik.arshad.dev@gmail.com>
2025-08-08 11:38:21 +0530

    Time: 2 ms (56.91%), Space: 41.5 MB (48.29%) - LeetHub

--- 2195-time-needed-to-buy-tickets/2195-time-needed-to-buy-tickets.java
+++ 2195-time-needed-to-buy-tickets/2195-time-needed-to-buy-tickets.java
@@ -1,28 +1,17 @@
 class Solution {
-    class Pair {
-        int tickets;
-        int index;
-        Pair(int tickets, int index) {
-                this.tickets = tickets;
-                this.index = index;
-        } 
-    }
     public int timeRequiredToBuy(int[] tickets, int k) {
-            Queue<Pair> queue = new LinkedList<>();
+        int n = tickets.length;
+        int time = 0;
+        while(tickets[k]!=0) {
             for(int i = 0;i < tickets.length;i++) {
-                    queue.offer(new Pair(tickets[i], i));
-            }
-            int time = 0;
-            while(!queue.isEmpty()) {
-                   Pair p = queue.poll();
-                   p.tickets--;
-                   time++;
-                   if(p.tickets>0) {
-                        queue.offer(p);
-                   } else if (p.tickets == 0) {
-                        if(p.index == k) return time;
+                   if(tickets[i]>0) {
+                        tickets[i]--;
+                        time++;
                    }
+                   if(tickets[k]==0) return time;
             }
-            return time;
+
+        }
+        return time;
     }
 }
