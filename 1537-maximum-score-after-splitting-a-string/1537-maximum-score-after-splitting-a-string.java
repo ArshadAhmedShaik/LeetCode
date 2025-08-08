25d2de482dffdfef1ff721f6b8bf20c7cb4cc9f5
Shaik Arshad Ahmed <shaik.arshad.dev@gmail.com>
2025-08-08 08:43:42 +0530

    Time: 1 ms (99.35%), Space: 41.4 MB (91.34%) - LeetHub

--- 1537-maximum-score-after-splitting-a-string/1537-maximum-score-after-splitting-a-string.java
+++ 1537-maximum-score-after-splitting-a-string/1537-maximum-score-after-splitting-a-string.java
@@ -1,29 +1,23 @@
 class Solution {
     public int maxScore(String s) {
-        int[] zeros = new int[s.length()];
-        int[] ones = new int[s.length()];
-        if(s.charAt(0)=='0') {
-                zeros[0] = 1;
-                ones[0] = 0;
-        } else {
-              zeros[0] = 0;
-              ones[0] = 1;
+        int totalOnes = 0;
+        for(var x: s.toCharArray()) {
+               if(x=='1') totalOnes++;
         }
-        for(int i = 1;i < zeros.length;i++) {
+        int leftZeros = 0;
+        int leftOnes = 0;
+        int score = 0;
+        int maxScore = -1;
+        for(int i = 0;i <= s.length()-2;i++) {
                 if(s.charAt(i)=='0') {
-                zeros[i] = zeros[i-1] + 1;
-                ones[i] = ones[i-1];
+                    leftZeros++;
                 } else {
-                    ones[i] = ones[i-1] + 1;
-                    zeros[i] = zeros[i-1];
+                    leftOnes++;
                 }
-         }
-         int maxScore = -1;
-         int score = 0;   
-         for(int i = 0;i < s.length()-1;i++) {
-                score = zeros[i] + ones[s.length()-1] - ones[i];
-                maxScore = Math.max(score, maxScore);
-         }
-         return maxScore;
+            score = leftZeros + (totalOnes - leftOnes);
+            maxScore = Math.max(score, maxScore);
+        }
+
+        return maxScore;
     }
 }
