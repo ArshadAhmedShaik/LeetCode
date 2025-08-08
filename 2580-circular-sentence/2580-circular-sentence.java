d65a06e3cb22964ae99537be17105d27a58e4e1e
Shaik Arshad Ahmed <shaik.arshad.dev@gmail.com>
2025-08-08 17:28:34 +0530

    Time: 1 ms (95.69%), Space: 42.2 MB (47.59%) - LeetHub

--- 2580-circular-sentence/2580-circular-sentence.java
+++ 2580-circular-sentence/2580-circular-sentence.java
@@ -1,15 +1,16 @@
 class Solution {
     public boolean isCircularSentence(String sentence) {
-        for(int i = 0;i < sentence.length();i++) {
-               char ch = sentence.charAt(i);
-                if(ch == ' ') {
-                        if(sentence.charAt(i-1) != sentence.charAt(i+1)) {
-                                return false;
-                        }
-                }
+        int n = sentence.length();
+        if (sentence.charAt(0) != sentence.charAt(n - 1)) {
+            return false;
         }
-        if(sentence.charAt(0)!=sentence.charAt(sentence.length()-1)) {
-                return false;
+        for (int i = 1; i < n - 1; i++) {
+            char c = sentence.charAt(i);
+            if (c == ' ') {
+                if (sentence.charAt(i - 1) != sentence.charAt(i + 1)) {
+                    return false;
+                }
+            }
         }
         return true;
     }
