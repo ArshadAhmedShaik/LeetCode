0689212e9dffc44df648b3af7ca047fd8289048c
Shaik Arshad Ahmed <shaik.arshad.dev@gmail.com>
2025-08-08 15:29:27 +0530

    Time: 7 ms (20.37%), Space: 42.1 MB (97.91%) - LeetHub

--- 1884-minimum-changes-to-make-alternating-binary-string/1884-minimum-changes-to-make-alternating-binary-string.java
+++ 1884-minimum-changes-to-make-alternating-binary-string/1884-minimum-changes-to-make-alternating-binary-string.java
@@ -1,11 +1,13 @@
 class Solution {
     public int minOperations(String s) {
-           int count = 0;
-           char cmp = '0';
-           for(char ch : s.toCharArray()) {
-                if(ch!=cmp) count++;
-                cmp = (char)('1' - cmp + '0');
-           }
-           return Math.min(count, s.length()-count);
+        int countStartWith0 = 0;
+        int countStartWith1 = 0;
+        for (int i = 0; i < s.length(); i++) {
+            char expected0 = (i % 2 == 0) ? '0' : '1';
+            char expected1 = (i % 2 == 0) ? '1' : '0';
+            if (s.charAt(i) != expected0) countStartWith0++;
+            if (s.charAt(i) != expected1) countStartWith1++;
+        }
+        return Math.min(countStartWith0, countStartWith1);
     }
 }
