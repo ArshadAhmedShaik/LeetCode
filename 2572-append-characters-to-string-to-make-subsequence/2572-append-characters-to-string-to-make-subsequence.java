42de90276c9df6bb3d9aa48943b1b098431f960b
Shaik Arshad Ahmed <shaik.arshad.dev@gmail.com>
2025-08-10 20:22:25 +0530

    Time: 5 ms (84.68%), Space: 45.5 MB (39.3%) - LeetHub

--- 2572-append-characters-to-string-to-make-subsequence/2572-append-characters-to-string-to-make-subsequence.java
+++ 2572-append-characters-to-string-to-make-subsequence/2572-append-characters-to-string-to-make-subsequence.java
@@ -4,16 +4,14 @@ class Solution {
         int right = 0;  // s
         int n1 = t.length();
         int n2 = s.length();
-        int count = 0;
         while(left<n1 && right<n2) {
             if(s.charAt(right)==t.charAt(left)) {
-                count++;
                 right++;
                 left++;
             } else {
                 right++;
             }
         }
-        return n1 - count;
+        return n1 - left;
     }
 }
