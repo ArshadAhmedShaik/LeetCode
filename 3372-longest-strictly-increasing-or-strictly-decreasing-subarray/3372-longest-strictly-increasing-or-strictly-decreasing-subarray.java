2be54f37a52196c1ee426cd7ccda8e05bdd01868
Shaik Arshad Ahmed <shaik.arshad.dev@gmail.com>
2025-08-10 15:53:15 +0530

    Time: 1 ms (91.94%), Space: 42.6 MB (70.86%) - LeetHub

--- 3372-longest-strictly-increasing-or-strictly-decreasing-subarray/3372-longest-strictly-increasing-or-strictly-decreasing-subarray.java
+++ 3372-longest-strictly-increasing-or-strictly-decreasing-subarray/3372-longest-strictly-increasing-or-strictly-decreasing-subarray.java
@@ -6,16 +6,17 @@ class Solution {
         int decrease = 1;
 
         for(int i = 1;i < nums.length;i++) {
-                if(nums[i]<=nums[i-1]) {
-                     increase = 1;
-                } else{
-                    increase += 1;
-                }
-                 if(nums[i]>=nums[i-1]) {
+               
+               if(nums[i]>nums[i-1]) {
+                    increase++;
                     decrease = 1;
-                } else {
-                    decrease += 1;
-                }
+               } else if(nums[i]<nums[i-1]) {
+                    decrease++;
+                    increase = 1;
+               } else {
+                    decrease = 1;
+                    increase = 1;
+               }
 
                 maxLength = Math.max(maxLength, Math.max(increase, decrease));
         }
