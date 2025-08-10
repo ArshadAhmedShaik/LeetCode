463d041fe62a87a9904bf8226897074cf8118404
Shaik Arshad Ahmed <shaik.arshad.dev@gmail.com>
2025-08-10 21:31:57 +0530

    Time: 4 ms (98.1%), Space: 85.9 MB (50.95%) - LeetHub

--- 2691-count-vowel-strings-in-ranges/2691-count-vowel-strings-in-ranges.java
+++ 2691-count-vowel-strings-in-ranges/2691-count-vowel-strings-in-ranges.java
@@ -1,47 +1,27 @@
 class Solution {
     public int[] vowelStrings(String[] words, int[][] queries) {
         int n = words.length;
+        int[] prefixCount = new int[n + 1];
+
+        for (int i = 0; i < n; i++) {
+            prefixCount[i + 1] = prefixCount[i] + (isVowel(words[i]) ? 1 : 0);
+        }
+
         int[] ans = new int[queries.length];
-        Set<Character> set = new HashSet<>(Arrays.asList('a','e','i','o','u'));
-        boolean[] isVowel = new boolean[n];
-        int[] count = new int[n];
-
-        for(int i = 0;i < n;i++) {
-            String str = words[i];
-            if(set.contains(str.charAt(0)) && set.contains(str.charAt(str.length()-1))) {
-                    isVowel[i] = true;
-            } else {
-                isVowel[i] = false;
-            }
-        }
-
-        if(isVowel[0]) {
-                count[0] = 1;
-        } else {
-            count[0] = 0; 
-        }
-       
-        for(int i = 1;i < count.length;i++) {
-                if(isVowel[i]) {
-                       count[i] = count[i-1] + 1; 
-                } else {
-                    count[i] = count[i-1];
-                }
-        }
-
-        int index = 0;
-
-        for(int i = 0;i < queries.length;i++) {
-                int start = queries[i][0];
-                int end = queries[i][1];
-                if(start==0) {
-                        ans[index++] = count[end];
-                } else {
-                        ans[index++] = count[end] - count[start-1];
-                }
+        for (int i = 0; i < queries.length; i++) {
+            int start = queries[i][0];
+            int end = queries[i][1];
+            ans[i] = prefixCount[end + 1] - prefixCount[start];
         }
 
         return ans;
+    }
 
+    private boolean isVowel(String word) {
+        return isVowelChar(word.charAt(0)) && isVowelChar(word.charAt(word.length() - 1));
+    }
+
+    private boolean isVowelChar(char c) {
+        return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u';
     }
 }
