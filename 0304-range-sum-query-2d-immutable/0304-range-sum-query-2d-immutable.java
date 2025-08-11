106a7cf6ce3ad9fb371cd4b5daf7851aa26d0ea8
Shaik Arshad Ahmed <shaik.arshad.dev@gmail.com>
2025-08-12 01:49:11 +0530

    Time: 101 ms (57.2%), Space: 71.4 MB (15.65%) - LeetHub

--- 0304-range-sum-query-2d-immutable/0304-range-sum-query-2d-immutable.java
+++ 0304-range-sum-query-2d-immutable/0304-range-sum-query-2d-immutable.java
@@ -1,34 +1,26 @@
 class NumMatrix {
-    
-    int[][] prefixMatrix;
+    int[][] prefix;
 
     public NumMatrix(int[][] matrix) {
-            int m = matrix.length;
+        int m = matrix.length;
         int n = matrix[0].length;
-        prefixMatrix = new int[m][n];
-        for(int i = 0;i < m;i++) {
-            for(int j = 0;j < n;j++) {
+        prefix = new int[m + 1][n + 1]; // 1-based indexing
 
-                int top = (i>0) ? prefixMatrix[i-1][j] : 0;
-                int left = (j>0) ? prefixMatrix[i][j-1] : 0;
-                int diagonal = (i>0 && j>0) ? prefixMatrix[i-1][j-1] : 0;
-
-                prefixMatrix[i][j] = matrix[i][j] + top + left - diagonal;
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                prefix[i][j] = matrix[i - 1][j - 1]
+                              + prefix[i - 1][j]
+                              + prefix[i][j - 1]
+                              - prefix[i - 1][j - 1];
             }
         }
     }
 
     public int sumRegion(int row1, int col1, int row2, int col2) {
-        int total = prefixMatrix[row2][col2];
-    int left = (col1 > 0) ? prefixMatrix[row2][col1 - 1] : 0;
-    int top = (row1 > 0) ? prefixMatrix[row1 - 1][col2] : 0;
-    int overlap = (row1 > 0 && col1 > 0) ? prefixMatrix[row1 - 1][col1 - 1] : 0;
-            return total - left - top + overlap;
+        row1++; col1++; row2++; col2++; 
+        return prefix[row2][col2]
+             - prefix[row1 - 1][col2]
+             - prefix[row2][col1 - 1]
+             + prefix[row1 - 1][col1 - 1];
     }
 }
-
-/**
- * Example usage:
- * NumMatrix obj = new NumMatrix(matrix);
- * int param_1 = obj.sumRegion(row1, col1, row2, col2);
- */
