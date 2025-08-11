9559f112f75f48d68af7c7644f77e32456fb5e65
Shaik Arshad Ahmed <shaik.arshad.dev@gmail.com>
2025-08-11 09:37:55 +0530

    Time: 69 ms (21.82%), Space: 55.2 MB (99.41%) - LeetHub

--- 0948-sort-an-array/0948-sort-an-array.java
+++ 0948-sort-an-array/0948-sort-an-array.java
@@ -1,24 +1,42 @@
 class Solution {
-    public int[] sortArray(int[] nums) {
-        int max = Integer.MIN_VALUE;
-        int min = Integer.MAX_VALUE;
-
-        for(var x: nums) {
-            max =  Math.max(max, x);
-            min =  Math.min(min, x);
-        }
-        int[] count = new int[max-min+1];
-        int offset = -min;
-        for(var x: nums){
-            count[x+offset]++;
-        }
-        int index = 0;
-        for(int i = 0;i < count.length;i++) {
-            while(count[i]!=0) {
-                nums[index++] = i - offset;
-                count[i]--;
+    private void merge(int[] nums, int low, int mid, int high) {
+            List<Integer> list = new ArrayList<>();
+            // merge low->mid and mid+1->high
+            int left = low;
+            int right = mid+1;
+            while(left <= mid && right <= high) {
+                    if(nums[left]<=nums[right]) {
+                        list.add(nums[left]);
+                        left++;
+                    } else {
+                        list.add(nums[right]);
+                        right++;
+                    }
             }
-        }
+            while(left<=mid) {
+                list.add(nums[left]);
+                left++;
+            }
+            while(right<=high) {
+                list.add(nums[right]);
+                right++;
+            }
+            int j = 0;
+            for(int i = low;i <= high;i++) {
+                    nums[i] = list.get(j++);
+            }
+    }
+    private void mergeSort(int[] nums,int low,int high) {
+                if(low>=high) {
+                        return;
+                }
+                int mid = (low+high)/2;
+                mergeSort(nums, low, mid);
+                mergeSort(nums, mid+1, high);
+                merge(nums, low, mid, high);
+    }
+    public int[] sortArray(int[] nums) {
+        mergeSort(nums, 0, nums.length-1);
         return nums;
     }
 }
