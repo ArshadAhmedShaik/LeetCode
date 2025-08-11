3b961c116996433b1c40d65ba62e25f09499b82e
Shaik Arshad Ahmed <shaik.arshad.dev@gmail.com>
2025-08-12 01:48:07 +0530

    Time: 103 ms (45.47%), Space: 71 MB (37.23%) - LeetHub

--- 0304-range-sum-query-2d-immutable/0304-range-sum-query-2d-immutable.java
+++ 0304-range-sum-query-2d-immutable/0304-range-sum-query-2d-immutable.java
@@ -1,33 +1,29 @@
 class NumMatrix {
-    int[][] rowPrefixSum;
-    int[][] colPrefixSum;
+    
+    int[][] prefixMatrix;
 
     public NumMatrix(int[][] matrix) {
-        int m = matrix.length;
+            int m = matrix.length;
         int n = matrix[0].length;
+        prefixMatrix = new int[m][n];
+        for(int i = 0;i < m;i++) {
+            for(int j = 0;j < n;j++) {
 
-        rowPrefixSum = new int[m][n];
-        colPrefixSum = new int[m][n];
+                int top = (i>0) ? prefixMatrix[i-1][j] : 0;
+                int left = (j>0) ? prefixMatrix[i][j-1] : 0;
+                int diagonal = (i>0 && j>0) ? prefixMatrix[i-1][j-1] : 0;
 
-        for (int i = 0; i < m; i++) {
-            rowPrefixSum[i][0] = matrix[i][0];
-            for (int j = 1; j < n; j++) {
-                rowPrefixSum[i][j] = rowPrefixSum[i][j - 1] + matrix[i][j];
+                prefixMatrix[i][j] = matrix[i][j] + top + left - diagonal;
             }
         }
-
     }
 
     public int sumRegion(int row1, int col1, int row2, int col2) {
-        int sum = 0;
-        for (int r = row1; r <= row2; r++) {
-            if (col1 == 0) {
-                sum += rowPrefixSum[r][col2];
-            } else {
-                sum += rowPrefixSum[r][col2] - rowPrefixSum[r][col1 - 1];
-            }
-        }
-        return sum;
+        int total = prefixMatrix[row2][col2];
+    int left = (col1 > 0) ? prefixMatrix[row2][col1 - 1] : 0;
+    int top = (row1 > 0) ? prefixMatrix[row1 - 1][col2] : 0;
+    int overlap = (row1 > 0 && col1 > 0) ? prefixMatrix[row1 - 1][col1 - 1] : 0;
+            return total - left - top + overlap;
     }
 }
 
