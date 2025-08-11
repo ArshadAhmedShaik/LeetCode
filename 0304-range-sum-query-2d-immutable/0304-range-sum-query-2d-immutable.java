331acf3f04e06a2adce2f3642d550f8af1fda803
Shaik Arshad Ahmed <shaik.arshad.dev@gmail.com>
2025-08-12 01:50:40 +0530

    Time: 97 ms (93.01%), Space: 71.6 MB (9.29%) - LeetHub

--- 0304-range-sum-query-2d-immutable/0304-range-sum-query-2d-immutable.java
+++ 0304-range-sum-query-2d-immutable/0304-range-sum-query-2d-immutable.java
@@ -1,26 +1,19 @@
 class NumMatrix {
-    int[][] prefix;
-
+    private int[][] sums;
+    
     public NumMatrix(int[][] matrix) {
-        int m = matrix.length;
-        int n = matrix[0].length;
-        prefix = new int[m + 1][n + 1]; // 1-based indexing
-
-        for (int i = 1; i <= m; i++) {
-            for (int j = 1; j <= n; j++) {
-                prefix[i][j] = matrix[i - 1][j - 1]
-                              + prefix[i - 1][j]
-                              + prefix[i][j - 1]
-                              - prefix[i - 1][j - 1];
-            }
-        }
+        int n = matrix.length;
+        int m = matrix[0].length;
+        sums = new int[n+1][m+1];
+        for (int i=1; i<=n; i++)
+            for (int j=1; j<=m; j++)
+                sums[i][j] = sums[i-1][j] + sums[i][j-1] + matrix[i-1][j-1] - sums[i-1][j-1];
     }
-
+    
     public int sumRegion(int row1, int col1, int row2, int col2) {
-        row1++; col1++; row2++; col2++; 
-        return prefix[row2][col2]
-             - prefix[row1 - 1][col2]
-             - prefix[row2][col1 - 1]
-             + prefix[row1 - 1][col1 - 1];
+        int RED_RECTANGLE = sums[row2+1][col2+1];
+        int PURPLE_RECTANGLES = sums[row1][col2+1] + sums[row2+1][col1];
+        int BLUE_RECTANGLE = sums[row1][col1];
+        return RED_RECTANGLE - PURPLE_RECTANGLES + BLUE_RECTANGLE;
     }
 }
