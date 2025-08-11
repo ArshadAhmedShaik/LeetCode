c344d820f83dee60c9d95de6a353e83e6977ab2e
Shaik Arshad Ahmed <shaik.arshad.dev@gmail.com>
2025-08-11 09:38:31 +0530

    Time: 2 ms (99.98%), Space: 56.2 MB (53.44%) - LeetHub

--- 0948-sort-an-array/0948-sort-an-array.java
+++ 0948-sort-an-array/0948-sort-an-array.java
@@ -1,42 +1,14 @@
 class Solution {
-    private void merge(int[] nums, int low, int mid, int high) {
-            List<Integer> list = new ArrayList<>();
-            // merge low->mid and mid+1->high
-            int left = low;
-            int right = mid+1;
-            while(left <= mid && right <= high) {
-                    if(nums[left]<=nums[right]) {
-                        list.add(nums[left]);
-                        left++;
-                    } else {
-                        list.add(nums[right]);
-                        right++;
-                    }
-            }
-            while(left<=mid) {
-                list.add(nums[left]);
-                left++;
-            }
-            while(right<=high) {
-                list.add(nums[right]);
-                right++;
-            }
-            int j = 0;
-            for(int i = low;i <= high;i++) {
-                    nums[i] = list.get(j++);
-            }
-    }
-    private void mergeSort(int[] nums,int low,int high) {
-                if(low>=high) {
-                        return;
-                }
-                int mid = (low+high)/2;
-                mergeSort(nums, low, mid);
-                mergeSort(nums, mid+1, high);
-                merge(nums, low, mid, high);
-    }
     public int[] sortArray(int[] nums) {
-        mergeSort(nums, 0, nums.length-1);
+        
+        Thread sortThread = new Thread(() -> Arrays.sort(nums));//Dual-Pivot QuickSort
+        sortThread.start();
+        try{
+            sortThread.join();
+
+        }
+        catch(Exception e){
+        }
         return nums;
     }
 }
