36e7fce06b7ebdb2e2adf60895ca7322561f6eec
Shaik Arshad Ahmed <shaik.arshad.dev@gmail.com>
2025-08-12 20:21:00 +0530

    Time: 3 ms (97.11%), Space: 44.6 MB (86.14%) - LeetHub

--- 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/1895-minimum-number-of-operations-to-move-all-balls-to-each-box.java
+++ 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/1895-minimum-number-of-operations-to-move-all-balls-to-each-box.java
@@ -1,19 +1,28 @@
 class Solution {
     public int[] minOperations(String boxes) {
-        int n = boxes.length();
-        char[] box = boxes.toCharArray();
-        int[] ans = new int[n];
-        for(int i = 0;i < n;i++) {
-            int sum = 0;
-            for(int j = 0;j < n;j++) {
-                if(i!=j) {
-                    if(box[j]=='1') {
-                        sum += Math.abs(i-j);
-                    }
-                }
-            }
-            ans[i] = sum;
-        }
-        return ans;
+          int n = boxes.length();
+          char[] box = boxes.toCharArray();
+          int[] left = new int[n];
+          int[] right = new int[n];
+          int countL = 0;
+          left[0] = 0;
+          for(int i = 1;i < n;i++) {
+                if(box[i-1]=='1') countL++;
+                left[i] = left[i-1] + countL;
+          }
+          right[n-1] = 0;
+          int countR = 0;
+          for(int i = n-2;i >= 0;i--) {
+                if(box[i+1]=='1') countR++;
+                right[i] = right[i+1] + countR;
+          }
+
+          int[] ans = new int[n];
+          for(int i = 0;i < n;i++) {
+                ans[i] = left[i] + right[i];
+          }
+
+          return ans;
+
     }
 }
