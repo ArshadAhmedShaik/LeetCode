479900d9ca1dc0ecf720849e9e2c2a75dcd2ed9c
Shaik Arshad Ahmed <shaik.arshad.dev@gmail.com>
2025-08-12 10:03:09 +0530

    Time: 2 ms (99.9%), Space: 45.3 MB (89.58%) - LeetHub

--- 2572-append-characters-to-string-to-make-subsequence/2572-append-characters-to-string-to-make-subsequence.java
+++ 2572-append-characters-to-string-to-make-subsequence/2572-append-characters-to-string-to-make-subsequence.java
@@ -1,5 +1,9 @@
 class Solution {
-    public int appendCharacters(String s, String t) {
+    static {
+        for(int i = 0;i < 500;i++)
+        appendCharacters(new String("hgjhg"), new String("hjfgk"));
+    }
+    public static int appendCharacters(String s, String t) {
         char[] arrS = s.toCharArray();
         char[] arrT = t.toCharArray();
         int left = 0, right = 0;
