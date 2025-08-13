9f36383e91470680379862fe605f8de6e563a846
Shaik Arshad Ahmed <shaik.arshad.dev@gmail.com>
2025-08-13 17:19:11 +0530

    Time: 2 ms (78.66%), Space: 44.2 MB (89.44%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -1,34 +1,28 @@
 public class Solution {
     public boolean isValidSudoku(char[][] board) {
 
-            Set<Character>[] rowSets = new HashSet[9];
-            Set<Character>[] colSets = new HashSet[9];
-            Set<Character>[] boxSets = new HashSet[9];
+            Set<Character>[] rowSet = new Set[9];
+            Set<Character>[] colSet = new Set[9];
+            Set<Character>[]  boxes = new Set[9];
 
-            for(int i = 0;i < 9;i++) {
-                    rowSets[i] = new HashSet<>();
-                    colSets[i] = new HashSet<>();
-                    boxSets[i] = new HashSet<>();
-            }
+             for(int i = 0;i < 9;i++) {
+                    rowSet[i] = new HashSet<>();
+                    colSet[i] = new HashSet<>();
+                    boxes[i] = new HashSet<>();
+             }
 
-            for(int i = 0;i < 9;i++) {
+             for(int i = 0;i < 9;i++) {
                 for(int j = 0;j < 9;j++) {
-                     char num = board[i][j];
-                     if(num=='.') continue;
-                     // row scanning
-                     if(!rowSets[i].add(num)) {
-                            return false;
-                     }
-                     // cols checking
-                     if(!colSets[j].add(num)) return false;
-
-                     // box checking
-
-                     int boxIndex = (i/3)*3 + (j/3);
-                     if(!boxSets[boxIndex].add(num)) return false;
+                        char ch = board[i][j];
+                        if(ch=='.') continue;
+                        if(!rowSet[i].add(ch)) return false;
+                        if(!colSet[j].add(ch)) return false;
+                        int boxIndex = (i/3)*3 + (j/3);
+                        if(!boxes[boxIndex].add(ch)) return false;
                 }
-            }
+             }
 
-            return true;
+             return true;
+            
     }
 }
