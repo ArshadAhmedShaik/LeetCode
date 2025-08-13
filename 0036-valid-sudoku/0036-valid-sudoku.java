e93314af6490058a728ccaf0a114c209b6c9ec00
Shaik Arshad Ahmed <shaik.arshad.dev@gmail.com>
2025-08-13 22:01:53 +0530

    Time: 2 ms (78.66%), Space: 44.2 MB (89.44%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -3,26 +3,24 @@ public class Solution {
 
             Set<Character>[] rowSet = new Set[9];
             Set<Character>[] colSet = new Set[9];
-            Set<Character>[]  boxes = new Set[9];
+            Set<Character>[] boxSet = new Set[9];
 
-             for(int i = 0;i < 9;i++) {
-                    rowSet[i] = new HashSet<>();
-                    colSet[i] = new HashSet<>();
-                    boxes[i] = new HashSet<>();
-             }
+            for(int i = 0;i < 9;i++) {
+                rowSet[i] = new HashSet<>();
+                colSet[i] = new HashSet<>();
+                boxSet[i] = new HashSet<>();
+            }
 
-             for(int i = 0;i < 9;i++) {
+            for(int i = 0;i < 9;i++) {
                 for(int j = 0;j < 9;j++) {
-                        char ch = board[i][j];
-                        if(ch=='.') continue;
-                        if(!rowSet[i].add(ch)) return false;
-                        if(!colSet[j].add(ch)) return false;
+                        if(board[i][j] == '.') continue;
+                        if(!rowSet[i].add(board[i][j])) return false;
+                        if(!colSet[j].add(board[i][j])) return false;
                         int boxIndex = (i/3)*3 + (j/3);
-                        if(!boxes[boxIndex].add(ch)) return false;
+                        if(!boxSet[boxIndex].add(board[i][j])) return false;
                 }
-             }
+            }
 
-             return true;
-            
+        return true;            
     }
 }
