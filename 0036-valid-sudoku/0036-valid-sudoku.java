09c0d201bbb12049f8436eac49877e6f5f6adb0b
Shaik Arshad Ahmed <shaik.arshad.dev@gmail.com>
2025-08-13 09:04:48 +0530

    Time: 2 ms (78.66%), Space: 44.9 MB (32.36%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -1,51 +1,37 @@
 class Solution {
     public boolean isValidSudoku(char[][] board) {
 
-          // row scanning
-          Set<Character> set = new HashSet<>();
-          for(int i = 0;i < 9;i++) {
-                char[] arr = board[i];
-                for(int j = 0;j < 9;j++) {
-                        if(arr[j] >= '1' && arr[j] <= '9') {
-                                if(!set.add(arr[j])) return false;
-                        }
-                }
-                set.clear();
-          }
-
-          set.clear();
-
-          // cols scanning
-
-          for(int j = 0;j < 9;j++) {
-                for(int i = 0;i < 9;i++) {
-                        char ch = board[i][j];
-                        if(ch >= '1' && ch <= '9') {
-                            if(!set.add(ch)) return false;
-                        }
-                }
-                set.clear();
-          }
-
-          set.clear();
-
-          // scanning each box:
-          
-          for(int startRow = 0;startRow < 9;startRow += 3) {
-            for(int startCol = 0;startCol < 9;startCol += 3) {
-                    for(int i = startRow;i < startRow+3;i++) {
-                        for(int j = startCol;j < startCol+3;j++) {
-                            char ch = board[i][j];
-                               if(ch <= '9' && ch >= '1') {
-                              if(!set.add(ch)) return false;
-                               }
-                        }
-                    }
-                    set.clear();
+         // row scanning 
+         Set<Character> set = new HashSet<>();
+         for(int i = 0;i < 9;i++) {
+            for(int j = 0;j < 9;j++) {
+                    if(board[i][j]=='.') continue; 
+                    if(!set.add(board[i][j])) return false;
             }
-          }
-          
-          return true;
+            set.clear();
+         }
+         // col scanning
+         for(int j = 0;j < 9;j++) {
+            for(int i = 0;i < 9;i++) {
+                    if(board[i][j]=='.') continue;
+                    if(!set.add(board[i][j])) return false;
+            }
+            set.clear();
+         }
+         // boxes scanning
+         for(int startRow = 0;startRow < 9;startRow += 3) {
+                for(int startCol = 0;startCol < 9;startCol += 3) {
+                        for(int i = startRow;i < startRow + 3;i++) {
+                                for(int j = startCol;j < startCol + 3;j++) {
+                                        if(board[i][j]=='.') continue;
+                                        if(!set.add(board[i][j])) return false;
+                                }
+                        }
+                        set.clear();
+                }
+         }
+
+         return true;
 
     }
 }
