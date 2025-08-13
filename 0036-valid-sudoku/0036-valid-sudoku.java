75613903119b7839ae1c5827f6e3a3bf2d1bc3fa
Shaik Arshad Ahmed <shaik.arshad.dev@gmail.com>
2025-08-14 01:48:45 +0530

    Time: 1 ms (99.91%), Space: 44.6 MB (57.41%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -1,30 +1,26 @@
 public class Solution {
     public boolean isValidSudoku(char[][] board) {
-
         int[] rowMask = new int[9];
         int[] colMask = new int[9];
         int[] boxMask = new int[9];
 
-        for(int i = 0;i < 9;i++) {
-            for(int j = 0;j < 9;j++) {
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                char c = board[i][j];
+                if (c == '.') continue;
 
-                    if(board[i][j] == '.') continue;
-                    int boxIndex = (i/3)*3 + (j/3);
+                int bit = 1 << (c - '0'); 
+                int box = (i / 3) * 3 + (j / 3);
 
-                    int num = board[i][j] - '0';
-                    int bit = 1 << num;
+                if ((rowMask[i] & bit) != 0 ||
+                    (colMask[j] & bit) != 0 ||
+                    (boxMask[box] & bit) != 0) return false;
 
-                    if((rowMask[i]&bit)!=0) return false;
-                    if((colMask[j]&bit)!=0) return false;
-                    if((boxMask[boxIndex]&bit)!=0) return false;
-
-                    rowMask[i] |= bit;
-                    colMask[j] |= bit;
-                    boxMask[boxIndex] |= bit;
-
+                rowMask[i] |= bit;
+                colMask[j] |= bit;
+                boxMask[box] |= bit;
             }
         }
-            
-        return true;            
+        return true;
     }
 }
