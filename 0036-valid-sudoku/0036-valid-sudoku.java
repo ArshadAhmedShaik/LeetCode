b86b0ca7edc3355547c74f8ced2e1ec8bc4e26fc
Shaik Arshad Ahmed <shaik.arshad.dev@gmail.com>
2025-08-14 01:47:46 +0530

    Time: 1 ms (99.91%), Space: 44.3 MB (89.4%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -1,26 +1,30 @@
 public class Solution {
     public boolean isValidSudoku(char[][] board) {
 
-            Set<Character>[] rowSet = new Set[9];
-            Set<Character>[] colSet = new Set[9];
-            Set<Character>[] boxSet = new Set[9];
+        int[] rowMask = new int[9];
+        int[] colMask = new int[9];
+        int[] boxMask = new int[9];
 
-            for(int i = 0;i < 9;i++) {
-                rowSet[i] = new HashSet<>();
-                colSet[i] = new HashSet<>();
-                boxSet[i] = new HashSet<>();
+        for(int i = 0;i < 9;i++) {
+            for(int j = 0;j < 9;j++) {
+
+                    if(board[i][j] == '.') continue;
+                    int boxIndex = (i/3)*3 + (j/3);
+
+                    int num = board[i][j] - '0';
+                    int bit = 1 << num;
+
+                    if((rowMask[i]&bit)!=0) return false;
+                    if((colMask[j]&bit)!=0) return false;
+                    if((boxMask[boxIndex]&bit)!=0) return false;
+
+                    rowMask[i] |= bit;
+                    colMask[j] |= bit;
+                    boxMask[boxIndex] |= bit;
+
             }
-
-            for(int i = 0;i < 9;i++) {
-                for(int j = 0;j < 9;j++) {
-                        if(board[i][j] == '.') continue;
-                        if(!rowSet[i].add(board[i][j])) return false;
-                        if(!colSet[j].add(board[i][j])) return false;
-                        int boxIndex = (i/3)*3 + (j/3);
-                        if(!boxSet[boxIndex].add(board[i][j])) return false;
-                }
-            }
-
+        }
+            
         return true;            
     }
 }
