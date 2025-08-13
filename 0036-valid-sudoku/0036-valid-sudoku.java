177c268055108db9a821f6989f458a19e89ad62f
Shaik Arshad Ahmed <shaik.arshad.dev@gmail.com>
2025-08-14 01:57:27 +0530

    Time: 3 ms (53.41%), Space: 44.3 MB (89.4%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -1,14 +1,25 @@
+import java.util.*;
+
 public class Solution {
     public boolean isValidSudoku(char[][] board) {
-        int[] row = new int[9], col = new int[9], box = new int[9];
+        Map<Integer, Set<Character>> row = new HashMap<>();
+        Map<Integer, Set<Character>> col = new HashMap<>();
+        Map<Integer, Set<Character>> box = new HashMap<>();
+
+        for (int i = 0; i < 9; i++) {
+            row.put(i, new HashSet<>());
+            col.put(i, new HashSet<>());
+            box.put(i, new HashSet<>());
+        }
+
         for (int i = 0; i < 9; i++) {
             for (int j = 0; j < 9; j++) {
                 char c = board[i][j];
                 if (c == '.') continue;
-                int bit = 1 << (c - '0');
-                int k = (i / 3) * 3 + j / 3;
-                if ((row[i] & bit) > 0 || (col[j] & bit) > 0 || (box[k] & bit) > 0) return false;
-                row[i] |= bit; col[j] |= bit; box[k] |= bit;
+                int boxIndex = (i / 3) * 3 + j / 3;
+                if (!row.get(i).add(c) || !col.get(j).add(c) || !box.get(boxIndex).add(c)) {
+                    return false;
+                }
             }
         }
         return true;
