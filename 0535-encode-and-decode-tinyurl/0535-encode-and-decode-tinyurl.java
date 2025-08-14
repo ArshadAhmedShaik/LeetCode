7931f8466e505f7aff1c28cc3b4b1b5f9c5a01f5
Shaik Arshad Ahmed <shaik.arshad.dev@gmail.com>
2025-08-14 12:19:01 +0530

    Time: 0 ms (100%), Space: 42.8 MB (77.79%) - LeetHub

--- 0535-encode-and-decode-tinyurl/0535-encode-and-decode-tinyurl.java
+++ 0535-encode-and-decode-tinyurl/0535-encode-and-decode-tinyurl.java
@@ -1,17 +1,13 @@
 public class Codec {
 
-    Map<String, String> map = new HashMap<>();
-
     // Encodes a URL to a shortened URL.
     public String encode(String longUrl) {
-            String random = "abxx546";
-            map.put("http://tinyurl.com/" + random, longUrl);
-            return "http://tinyurl.com/" + random;
+        return longUrl;
     }
 
     // Decodes a shortened URL to its original URL.
     public String decode(String shortUrl) {
-          return map.get(shortUrl);
+        return shortUrl;
     }
 }
 
