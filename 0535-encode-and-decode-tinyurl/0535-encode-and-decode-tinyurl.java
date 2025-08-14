5d714b8b498944a7ec3441b140483118497f473d
Shaik Arshad Ahmed <shaik.arshad.dev@gmail.com>
2025-08-14 12:20:04 +0530

    Time: 4 ms (45.66%), Space: 43.2 MB (34.23%) - LeetHub

--- 0535-encode-and-decode-tinyurl/0535-encode-and-decode-tinyurl.java
+++ 0535-encode-and-decode-tinyurl/0535-encode-and-decode-tinyurl.java
@@ -1,16 +1,36 @@
-public class Codec {
+import java.util.*;
 
-    // Encodes a URL to a shortened URL.
-    public String encode(String longUrl) {
-        return longUrl;
+public class Codec {
+    private static final String BASE_URL = "http://tinyurl.com/";
+    private static final String CHARACTERS = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    private static final int CODE_LENGTH = 6;
+    private Map<String, String> map = new HashMap<>();
+    private Random random = new Random();
+
+    // Generates a random code of length CODE_LENGTH
+    private String getRandomCode() {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < CODE_LENGTH; i++) {
+            sb.append(CHARACTERS.charAt(random.nextInt(CHARACTERS.length())));
+        }
+        return sb.toString();
     }
 
-    // Decodes a shortened URL to its original URL.
-    public String decode(String shortUrl) {
+    // Encodes a URL to a shortened URL
+    public String encode(String longUrl) {
+        String code;
+        // Make sure we don't generate a duplicate code
+        do {
+            code = getRandomCode();
+        } while (map.containsKey(BASE_URL + code));
+
+        String shortUrl = BASE_URL + code;
+        map.put(shortUrl, longUrl);
         return shortUrl;
     }
-}
 
-// Your Codec object will be instantiated and called as such:
-// Codec codec = new Codec();
-// codec.decode(codec.encode(url));
+    // Decodes a shortened URL to its original URL
+    public String decode(String shortUrl) {
+        return map.get(shortUrl);
+    }
+}
