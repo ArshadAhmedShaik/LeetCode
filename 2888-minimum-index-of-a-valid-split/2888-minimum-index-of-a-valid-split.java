6b1f0af8bbb59692089d29171f0c2357e17883a1
Shaik Arshad Ahmed <shaik.arshad.dev@gmail.com>
2025-08-16 20:31:19 +0530

    Time: 11 ms (88.44%), Space: 56.3 MB (94.15%) - LeetHub

--- 2888-minimum-index-of-a-valid-split/2888-minimum-index-of-a-valid-split.java
+++ 2888-minimum-index-of-a-valid-split/2888-minimum-index-of-a-valid-split.java
@@ -1,42 +1,33 @@
 class Solution {
     public int minimumIndex(List<Integer> nums) {
-
         int n = nums.size();
         int half = n / 2;
-
-       int majority = -1;
-       int count = 0;
-
-       for(int x: nums) {
-            if(x==majority) {
+        int majority = -1, count = 0;
+        for (int x : nums) {
+            if (x == majority) {
                 count++;
-            } else if(count == 0) {
+            } else if (count == 0) {
                 majority = x;
+                count = 1;
             } else {
                 count--;
             }
-       }
+        }
+        int dominant = majority;
 
-       int dominant = majority;
-
-      
-        int[] prefix = new int[n];
-        count = 0;
-
-        for (int i = 0; i < n; i++) {
-            if (nums.get(i) == dominant) count++;
-            prefix[i] = count;
+        
+        int total = 0;
+        for (int x : nums) {
+            if (x == dominant) total++;
         }
 
-        int total = prefix[n - 1]; 
-
-       
+        
+        int leftCount = 0;
         for (int i = 0; i < n - 1; i++) {
+            if (nums.get(i) == dominant) leftCount++;
 
-            int leftCount = prefix[i];
             int leftLen = i + 1;
-
-            int rightCount = total - prefix[i];
+            int rightCount = total - leftCount;
             int rightLen = n - leftLen;
 
             if (leftCount > leftLen / 2 && rightCount > rightLen / 2) {
