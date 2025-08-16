56c8eef500e07269e636a52dd9809184b6b00093
Shaik Arshad Ahmed <shaik.arshad.dev@gmail.com>
2025-08-16 20:28:01 +0530

    Time: 9 ms (95.69%), Space: 63.1 MB (15.87%) - LeetHub

--- 2888-minimum-index-of-a-valid-split/2888-minimum-index-of-a-valid-split.java
+++ 2888-minimum-index-of-a-valid-split/2888-minimum-index-of-a-valid-split.java
@@ -1,30 +1,38 @@
 class Solution {
     public int minimumIndex(List<Integer> nums) {
+
         int n = nums.size();
         int half = n / 2;
 
-        // Step 1: Find the dominant element
-        Map<Integer, Integer> freq = new HashMap<>();
-        int dominant = -1;
-        for (int x : nums) {
-            freq.put(x, freq.getOrDefault(x, 0) + 1);
-            if (freq.get(x) > half) {
-                dominant = x;
+       int majority = -1;
+       int count = 0;
+
+       for(int x: nums) {
+            if(x==majority) {
+                count++;
+            } else if(count == 0) {
+                majority = x;
+            } else {
+                count--;
             }
-        }
+       }
+
+       int dominant = majority;
 
       
         int[] prefix = new int[n];
-        int count = 0;
+        count = 0;
+
         for (int i = 0; i < n; i++) {
             if (nums.get(i) == dominant) count++;
             prefix[i] = count;
         }
+
         int total = prefix[n - 1]; 
 
        
         for (int i = 0; i < n - 1; i++) {
-            
+
             int leftCount = prefix[i];
             int leftLen = i + 1;
 
