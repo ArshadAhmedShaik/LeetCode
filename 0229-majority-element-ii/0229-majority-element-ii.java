c6dfc9b15781a1175557eb26ae75c4b45442d9fd
Shaik Arshad Ahmed <shaik.arshad.dev@gmail.com>
2025-08-16 17:03:56 +0530

    Time: 3 ms (76.59%), Space: 50.5 MB (27.09%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,46 +1,25 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
+        int n = nums.length;
+        int cand1 = 0, cand2 = 0, count1 = 0, count2 = 0;
 
-      List<Integer> list =  new ArrayList<>();
-      int majority1 = Integer.MIN_VALUE;
-      int majority2 = Integer.MIN_VALUE;
-      int count1 = 0;
-      int count2 = 0;
-      for(int i = 0;i < nums.length;i++) {
-            if(count1 == 0 && nums[i]!=majority2) {
-                    count1++;
-                    majority1 = nums[i];
-            } else if(count2 == 0 && nums[i]!=majority1) {
-                    count2++;
-                    majority2 = nums[i];
-            } else if(majority1==nums[i]) {
-                    count1++;
-            } else if(majority2==nums[i]) {
-                    count2++;
-            } else {
-                   count1--;
-                   count2--;
-            }
-      }
+        for (int num : nums) {
+            if (num == cand1) count1++;
+            else if (num == cand2) count2++;
+            else if (count1 == 0) { cand1 = num; count1 = 1; }
+            else if (count2 == 0) { cand2 = num; count2 = 1; }
+            else { count1--; count2--; }
+        }
 
-      // manual check
+        count1 = count2 = 0;
+        for (int num : nums) {
+            if (num == cand1) count1++;
+            else if (num == cand2) count2++;
+        }
 
-      count1 = 0;
-      count2 = 0;
-
-      for(int i = 0;i < nums.length;i++) {
-            if(nums[i]==majority1) count1++;
-            if(nums[i]==majority2) count2++;
-      }
-
-      if(count1>(nums.length/3)) {
-            list.add(majority1);
-      } 
-      if(count2>(nums.length/3)) {
-        list.add(majority2);
-      }
-
-      return list;
-           
+        List<Integer> res = new ArrayList<>();
+        if (count1 > n / 3) res.add(cand1);
+        if (count2 > n / 3) res.add(cand2);
+        return res;
     }
 }
