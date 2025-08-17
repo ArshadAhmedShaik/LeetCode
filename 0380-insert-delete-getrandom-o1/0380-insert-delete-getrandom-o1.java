fa40a481221e74377cc67ca9906e9583319a251f
Shaik Arshad Ahmed <shaik.arshad.dev@gmail.com>
2025-08-17 16:07:39 +0530

    Time: 59 ms (17.39%), Space: 92.6 MB (20.62%) - LeetHub

--- 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.java
+++ 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.java
@@ -3,22 +3,25 @@ import java.util.*;
 class RandomizedSet {
     private Set<Integer> set;
     private Random rand;
+    private List<Integer> list;
 
     public RandomizedSet() {
         set = new HashSet<>();
         rand = new Random();
+        list = new ArrayList<>();
     }
 
     public boolean insert(int val) {
+        if(!set.contains(val)) list.add(val); 
         return set.add(val);
     }
 
     public boolean remove(int val) {
+        if(set.contains(val)) list.remove(Integer.valueOf(val));
         return set.remove(val);
     }
 
     public int getRandom() {
-        List<Integer> list = new ArrayList<>(set);
-        return list.get(rand.nextInt(0, list.size()));
+        return list.get(rand.nextInt(list.size()));
     }
 }
