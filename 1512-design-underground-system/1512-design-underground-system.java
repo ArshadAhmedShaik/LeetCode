7913071e64f4e7147fd88ccbd990f1ed60a2854d
Shaik Arshad Ahmed <shaik.arshad.dev@gmail.com>
2025-08-18 18:52:47 +0530

    Time: 92 ms (81.5%), Space: 55.1 MB (83.43%) - LeetHub

--- 1512-design-underground-system/1512-design-underground-system.java
+++ 1512-design-underground-system/1512-design-underground-system.java
@@ -1,18 +1,11 @@
 class UndergroundSystem {
 
-    // id -> checkIn time -> starting place
-    // 89 -> (56, hyd)
-    // 89 -> 60 -> bang
-    // (starting Place, Ending place) -> totalTime
-    // if map1.get(id).place = hyd
-    // (hyd, bang) -> 
-
     class CheckIn {
         String place;
         int time;
         CheckIn(String place, int time) {
-                this.place = place;
-                this.time = time;
+            this.place = place;
+            this.time = time;
         }
     }
 
@@ -23,28 +16,23 @@ class UndergroundSystem {
         map1 = new HashMap<>();
         map2 = new HashMap<>();
     }
-    
-    public void checkIn(int id, String stationName, int t) {
-           map1.put(id, new CheckIn(stationName, t));
-    }
-    
-    public void checkOut(int id, String stationName, int t) {
-         String obj = map1.get(id).place +"|"+ stationName;
-         map2.putIfAbsent(obj, new int[2]);
-         map2.get(obj)[0] += (t - map1.get(id).time); 
-         map2.get(obj)[1] += 1;                      
 
+    public void checkIn(int id, String stationName, int t) {
+        map1.put(id, new CheckIn(stationName, t));
     }
-    
+
+    public void checkOut(int id, String stationName, int t) {
+        CheckIn checkIn = map1.remove(id);
+        String key = checkIn.place + "|" + stationName;
+
+        map2.putIfAbsent(key, new int[2]);
+        map2.get(key)[0] += (t - checkIn.time); 
+        map2.get(key)[1] += 1;                 
+    }
+
     public double getAverageTime(String startStation, String endStation) {
-             return (double)map2.get(startStation+"|"+endStation)[0]/map2.get(startStation+"|"+endStation)[1];
+        String key = startStation + "|" + endStation;
+        int[] data = map2.get(key);
+        return (double) data[0] / data[1];
     }
 }
-
-/**
- * Your UndergroundSystem object will be instantiated and called as such:
- * UndergroundSystem obj = new UndergroundSystem();
- * obj.checkIn(id,stationName,t);
- * obj.checkOut(id,stationName,t);
- * double param_3 = obj.getAverageTime(startStation,endStation);
- */
