5b54f3f0762734f64d6b2a725b63de0459573c76
Shaik Arshad Ahmed <shaik.arshad.dev@gmail.com>
2025-08-18 17:14:37 +0530

    Time: 196 ms (63.61%), Space: 74 MB (62.6%) - LeetHub

--- 2429-design-a-food-rating-system/2429-design-a-food-rating-system.java
+++ 2429-design-a-food-rating-system/2429-design-a-food-rating-system.java
@@ -1,50 +1,41 @@
 class FoodRatings {
 
     class Food {
-        String name;
+        String name, cuisine;
         int rating;
-        Food(String name, int rating) {
+
+        Food(String name, String cuisine, int rating) {
             this.name = name;
+            this.cuisine = cuisine;
             this.rating = rating;
         }
     }
 
-    Map<String, Integer> ftor;
-    Map<String, String> ftoc;
-    Map<String, TreeSet<Food>> ctof;
     Map<String, Food> foodObj;
+    Map<String, TreeSet<Food>> ctof;
 
     public FoodRatings(String[] foods, String[] cuisines, int[] ratings) {
-        ftor = new HashMap<>();
-        ftoc = new HashMap<>();
-        ctof = new HashMap<>();
         foodObj = new HashMap<>();
+        ctof = new HashMap<>();
 
         int n = foods.length;
         for (int i = 0; i < n; i++) {
-            ftor.put(foods[i], ratings[i]);
-            ftoc.put(foods[i], cuisines[i]);
-            Food f = new Food(foods[i], ratings[i]);
+            Food f = new Food(foods[i], cuisines[i], ratings[i]);
             foodObj.put(foods[i], f);
 
             ctof.computeIfAbsent(cuisines[i], k -> new TreeSet<>(
-                (a, b) -> {
-                    if (a.rating != b.rating) return b.rating - a.rating;
-                    return a.name.compareTo(b.name);
-                }
+                (a, b) -> a.rating != b.rating ? b.rating - a.rating : a.name.compareTo(b.name)
             )).add(f);
         }
     }
 
     public void changeRating(String food, int newRating) {
-        String cuisine = ftoc.get(food);
-        Food oldFood = foodObj.get(food);
-        ctof.get(cuisine).remove(oldFood);
+        Food f = foodObj.get(food);
+        TreeSet<Food> set = ctof.get(f.cuisine);
 
-        Food newFood = new Food(food, newRating);
-        foodObj.put(food, newFood);
-        ftor.put(food, newRating);
-        ctof.get(cuisine).add(newFood);
+        set.remove(f);
+        f.rating = newRating;  
+        set.add(f);
     }
 
     public String highestRated(String cuisine) {
