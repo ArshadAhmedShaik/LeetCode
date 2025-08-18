07c7b64fd47624a7fae8ccc0162b25eb5b8bfa00
Shaik Arshad Ahmed <shaik.arshad.dev@gmail.com>
2025-08-18 17:13:13 +0530

    Time: 219 ms (40.97%), Space: 73.2 MB (76.08%) - LeetHub

--- 2429-design-a-food-rating-system/2429-design-a-food-rating-system.java
+++ 2429-design-a-food-rating-system/2429-design-a-food-rating-system.java
@@ -1,6 +1,6 @@
 class FoodRatings {
 
-     class Food {
+    class Food {
         String name;
         int rating;
         Food(String name, int rating) {
@@ -11,71 +11,43 @@ class FoodRatings {
 
     Map<String, Integer> ftor;
     Map<String, String> ftoc;
-    Map<String, PriorityQueue<Food>> ctof;
-
-   
+    Map<String, TreeSet<Food>> ctof;
+    Map<String, Food> foodObj;
 
     public FoodRatings(String[] foods, String[] cuisines, int[] ratings) {
         ftor = new HashMap<>();
         ftoc = new HashMap<>();
         ctof = new HashMap<>();
+        foodObj = new HashMap<>();
 
         int n = foods.length;
         for (int i = 0; i < n; i++) {
             ftor.put(foods[i], ratings[i]);
-        }
-        for (int i = 0; i < n; i++) {
             ftoc.put(foods[i], cuisines[i]);
-        }
+            Food f = new Food(foods[i], ratings[i]);
+            foodObj.put(foods[i], f);
 
-        for (var x : ftor.entrySet()) {
-            String food = x.getKey();
-            int rating = x.getValue();
-            String cuisine = ftoc.get(food);
-
-            ctof.computeIfAbsent(cuisine, k -> new PriorityQueue<>(
-                    (a, b) -> {
-                        if (a.rating < b.rating) {
-                            return 1;
-                        } else if (a.rating > b.rating) {
-                            return -1;
-                        } else {
-                            // both are equal
-                            return a.name.compareTo(b.name);
-                        }
-                    }));
-
-            ctof.get(cuisine).offer(new Food(food, rating));
-
+            ctof.computeIfAbsent(cuisines[i], k -> new TreeSet<>(
+                (a, b) -> {
+                    if (a.rating != b.rating) return b.rating - a.rating;
+                    return a.name.compareTo(b.name);
+                }
+            )).add(f);
         }
     }
 
     public void changeRating(String food, int newRating) {
+        String cuisine = ftoc.get(food);
+        Food oldFood = foodObj.get(food);
+        ctof.get(cuisine).remove(oldFood);
 
+        Food newFood = new Food(food, newRating);
+        foodObj.put(food, newFood);
         ftor.put(food, newRating);
-        ctof.get(ftoc.get(food)).offer(new Food(food, newRating));
-
+        ctof.get(cuisine).add(newFood);
     }
 
     public String highestRated(String cuisine) {
-
-        PriorityQueue<Food> q = ctof.get(cuisine);
-
-        while (!q.isEmpty()) {
-            Food top = q.peek();
-            if (top.rating == ftor.get(top.name)) {
-                return top.name;
-            }
-            q.poll();
-        }
-        return "";
-
+        return ctof.get(cuisine).first().name;
     }
 }
-
-/**
- * Your FoodRatings object will be instantiated and called as such:
- * FoodRatings obj = new FoodRatings(foods, cuisines, ratings);
- * obj.changeRating(food,newRating);
- * String param_2 = obj.highestRated(cuisine);
- */
