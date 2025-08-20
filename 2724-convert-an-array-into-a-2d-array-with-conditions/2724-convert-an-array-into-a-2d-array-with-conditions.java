5f3062c704c4be2e5cd2aa70b45964ffcee5c235
Shaik Arshad Ahmed <shaik.arshad.dev@gmail.com>
2025-08-20 14:01:08 +0530

    Time: 2 ms (90.76%), Space: 45.2 MB (32.77%) - LeetHub

--- 2724-convert-an-array-into-a-2d-array-with-conditions/2724-convert-an-array-into-a-2d-array-with-conditions.java
+++ 2724-convert-an-array-into-a-2d-array-with-conditions/2724-convert-an-array-into-a-2d-array-with-conditions.java
@@ -1,17 +1,15 @@
 class Solution {
     public List<List<Integer>> findMatrix(int[] nums) {
-       int[] count = new int[201];
-       for(int x: nums) count[x]++;
-       List<List<Integer>> ans = new ArrayList<>();
-       for(int i = 1;i <= 200;i++) {
-            int freq = count[i];
-            for(int row = 0;row < freq;row++) {
-                     if (ans.size() <= row) {
-                    ans.add(new ArrayList<>());
-                }   
-                ans.get(row).add(i);
+        List<List<Integer>> ans = new ArrayList<>();
+        int[] freq = new int[201];
+        
+        for (int x : nums) {
+            int row = freq[x]++;
+            if (ans.size() <= row) {
+                ans.add(new ArrayList<>());
             }
-       }
-       return ans;
+            ans.get(row).add(x);
+        }
+        return ans;
     }
 }
