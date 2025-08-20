c7de25e9d15867481b967d50984699b1d06e36ad
Shaik Arshad Ahmed <shaik.arshad.dev@gmail.com>
2025-08-20 12:31:03 +0530

    Time: 4 ms (49.08%), Space: 45.4 MB (9.92%) - LeetHub

---
+++ 2724-convert-an-array-into-a-2d-array-with-conditions/2724-convert-an-array-into-a-2d-array-with-conditions.java
@@ -0,0 +1,29 @@
+class Solution {
+    private boolean isEmpty(int[] count) {
+            for(int i = 0;i < count.length;i++) {
+                    if(count[i]!=0) return false; 
+            }
+            return true;
+    }
+    public List<List<Integer>> findMatrix(int[] nums) {
+            int[] count = new int[201];
+
+            for(int x: nums) {
+                count[x]++;
+            }
+
+            List<List<Integer>> ans = new ArrayList<>();
+
+            while(!isEmpty(count)) {
+            List<Integer> list = new ArrayList<>();
+            for(int i = 1;i <= 200;i++) {
+                    if(count[i]!=0) {
+                        list.add(i);
+                        count[i]--;
+                    }
+            }
+            ans.add(list);
+        } 
+            return ans;
+    }
+}
