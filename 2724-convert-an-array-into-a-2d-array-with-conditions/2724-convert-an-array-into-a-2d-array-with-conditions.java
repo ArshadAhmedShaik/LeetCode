c63b9ed5660a27638e402ba0b32a4baf0869b1b1
Shaik Arshad Ahmed <shaik.arshad.dev@gmail.com>
2025-08-20 13:55:18 +0530

    Time: 3 ms (72.27%), Space: 44.8 MB (85.71%) - LeetHub

--- 2724-convert-an-array-into-a-2d-array-with-conditions/2724-convert-an-array-into-a-2d-array-with-conditions.java
+++ 2724-convert-an-array-into-a-2d-array-with-conditions/2724-convert-an-array-into-a-2d-array-with-conditions.java
@@ -1,25 +1,28 @@
 class Solution {
     public List<List<Integer>> findMatrix(int[] nums) {
         int[] count = new int[201];
-        int maxFreq = 0;
-
-        for (int x : nums) {
+        for(int x: nums) {
             count[x]++;
-            maxFreq = Math.max(maxFreq, count[x]);
         }
-
+        int maxCount = 0;
+        int maxIndex = -1;
+        for(int i = 1;i <= 200;i++) {
+                if(count[i]>maxCount) {
+                        maxCount = count[i];
+                        maxIndex = i;
+                }
+        }
         List<List<Integer>> ans = new ArrayList<>();
-        for (int i = 0; i < maxFreq; i++) {
-            ans.add(new ArrayList<>());
+        while(count[maxIndex]!=0) {
+            List<Integer> list = new ArrayList<>();
+                for(int i = 1;i <= 200;i++) {
+                        if(count[i]!=0) {
+                            list.add(i);
+                            count[i]--;
+                        }
+                }
+                ans.add(list);
         }
-
-        for (int i = 1; i <= 200; i++) {
-            int freq = count[i];
-            for (int j = 0; j < freq; j++) {
-                ans.get(j).add(i);
-            }
-        }
-
         return ans;
     }
 }
