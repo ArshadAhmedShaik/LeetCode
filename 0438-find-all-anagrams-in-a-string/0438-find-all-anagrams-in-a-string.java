e4334063055251c471e74139b43d09b1cf0d1a16
Shaik Arshad Ahmed <shaik.arshad.dev@gmail.com>
2025-08-23 20:35:47 +0530

    Time: 349 ms (16.03%), Space: 46.2 MB (7.41%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
@@ -3,18 +3,24 @@ class Solution {
         List<Integer> ans = new ArrayList<>();
         int n = p.length();
         int m = s.length();
+        int[] count = new int[26];
+
+        for(int i = 0;i < n;i++) {
+                count[p.charAt(i)-'a']++;
+        }
+
         for(int i = 0;i <= m - n;i++) {
             String sub = s.substring(i, i+n);
-            int[] count = new int[26];
+            int[] count2 = new int[26];
             for(int j = 0;j < n;j++) {
-                    count[sub.charAt(j)-'a']++;
-                    count[p.charAt(j)-'a']--;
+                    count2[sub.charAt(j)-'a']++;
             }
+
             boolean isAnagram = true;
-            for(int x: count) {
-                if(x!=0) {
-                    isAnagram = false;
-                    break;
+            for(int j = 0;j < 26;j++) {
+                if(count[j]!=count2[j]) {
+                        isAnagram = false;
+                        break;
                 }
             }
 
