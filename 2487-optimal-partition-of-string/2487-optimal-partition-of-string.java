93033db4424adb585e6766b680e90533faf1c8fe
Shaik Arshad Ahmed <shaik.arshad.dev@gmail.com>
2025-08-23 17:18:48 +0530

    Time: 4 ms (100%), Space: 45.3 MB (73.97%) - LeetHub

--- 2487-optimal-partition-of-string/2487-optimal-partition-of-string.java
+++ 2487-optimal-partition-of-string/2487-optimal-partition-of-string.java
@@ -1,20 +1,19 @@
 class Solution {
     public int partitionString(String s) {
-            
-          int count = 1;
-          int mask = 0;
-          int n = s.length();
-          for(int i = 0;i < n;i++) {
-                char ch = s.charAt(i);
-                // if it is absent
-                int k = 1 << (ch-'a');
-                if((mask&k) == 0) {
-                    mask |= k;
-                } else { // if it is present
-                        count++;
-                        mask = k;
-                }
-          }  
+        int count = 1;
+        int mask = 0;
+        char[] arr = s.toCharArray();
+
+        for (char ch : arr) {
+            int k = 1 << (ch - 'a');
+            if ((mask & k) == 0) {
+                mask |= k;
+            } else {
+                count++;
+                mask = k;
+            }
+        }
+
         return count;
     }
 }
