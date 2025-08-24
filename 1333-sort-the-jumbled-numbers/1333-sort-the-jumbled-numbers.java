39826601e3745a62e2f7bd0332b736e920338bc0
Shaik Arshad Ahmed <shaik.arshad.dev@gmail.com>
2025-08-24 20:28:31 +0530

    Time: 1753 ms (5.45%), Space: 55.4 MB (90.1%) - LeetHub

--- 1333-sort-the-jumbled-numbers/1333-sort-the-jumbled-numbers.java
+++ 1333-sort-the-jumbled-numbers/1333-sort-the-jumbled-numbers.java
@@ -1,34 +1,38 @@
 public class Solution {
-    public int[] sortJumbled(int[] mapping, int[] nums) {
-        int n = nums.length;
-        int[][] pairs = new int[n][2];
 
-        for (int i = 0; i < n; i++) {
-            int mapped_n = 0, base = 1;
-            int num = nums[i];
-
-            if (num == 0) {
-                mapped_n = mapping[0];
-            } else {
-                while (num > 0) {
-                    int digit = num % 10;
-                    num /= 10;
-                    mapped_n += base * mapping[digit];
-                    base *= 10;
-                }
+    private int map(int num, int[] mapping) {
+            String str = String.valueOf(num);
+            StringBuilder sb = new StringBuilder();
+            for(char ch: str.toCharArray()) {
+                    int digit = mapping[ch - '0'];
+                    sb.append((char)(digit + '0'));
             }
+            return Integer.valueOf(sb.toString());
+    }    
 
-            pairs[i][0] = mapped_n;
-            pairs[i][1] = i;
-        }
+    public int[] sortJumbled(int[] mapping, int[] nums) {
+                
+          int n = nums.length;      
+          Integer[] arr = new Integer[n];
+          for(int i = 0;i < n;i++) {
+                arr[i] = nums[i];
+          }    
 
-        Arrays.sort(pairs, (a, b) -> Integer.compare(a[0], b[0]));
+          Arrays.sort(arr, new Comparator<Integer>() {
+                    public int compare(Integer a, Integer b) {
+                            int num1 = map(a, mapping);
+                            int num2 = map(b, mapping);
+                            if(num1>num2) {
+                                return 1;
+                            } else if(num2>num1) {
+                                return -1;
+                            } else {
+                                return 0;
+                            }
+                    }
+          });
 
-        int[] res = new int[n];
-        for (int i = 0; i < n; i++) {
-            res[i] = nums[pairs[i][1]];
-        }
+          return Arrays.stream(arr).mapToInt(Integer::intValue).toArray();
 
-        return res;
     }
-}
+} 
