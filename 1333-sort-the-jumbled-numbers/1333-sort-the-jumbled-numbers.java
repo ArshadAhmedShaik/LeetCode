ac16eb6833d7d1a95171e38b2e166056d1b4e030
Shaik Arshad Ahmed <shaik.arshad.dev@gmail.com>
2025-08-24 21:39:08 +0530

    Time: 90 ms (80.2%), Space: 56.1 MB (14.85%) - LeetHub

--- 1333-sort-the-jumbled-numbers/1333-sort-the-jumbled-numbers.java
+++ 1333-sort-the-jumbled-numbers/1333-sort-the-jumbled-numbers.java
@@ -1,48 +1,40 @@
+import java.util.*;
+
 public class Solution {
 
     private int map(int num, int[] mapping) {
+        if (num == 0) return mapping[0]; // edge case
 
-        if (num == 0)
-            return mapping[0];
-
-        int res = 0;
-        int place = 1;
+        int res = 0, place = 1;
         while (num > 0) {
             int digit = num % 10;
-            res += (mapping[digit]) * place;
+            res += mapping[digit] * place;
             place *= 10;
             num /= 10;
         }
-
         return res;
-
     }
 
     public int[] sortJumbled(int[] mapping, int[] nums) {
-
         int n = nums.length;
-        int[][] pairs = new int[n][2];
+        int[][] arr = new int[n][2];
 
+      
         for (int i = 0; i < n; i++) {
-            pairs[i][0] = map(nums[i], mapping);
-            pairs[i][1] = nums[i];
+            arr[i][0] = map(nums[i], mapping); 
+            arr[i][1] = i;                     
         }
 
-        // Arrays.sort(pairs, new Comparator<int[]>() {
-        //         public int compare(int[] a, int[] b) {
-        //             return Integer.compare(a[0], b[0]);
-        //         }
-        // }
-
-        Arrays.sort(pairs, (a, b) -> Integer.compare(a[0], b[0]));
+        Arrays.sort(arr, (a, b) -> {
+            if (a[0] != b[0]) return Integer.compare(a[0], b[0]);
+            return Integer.compare(a[1], b[1]); 
+        });
 
         int[] res = new int[n];
-
         for (int i = 0; i < n; i++) {
-            res[i] = pairs[i][1];
+            res[i] = nums[arr[i][1]];
         }
 
         return res;
-
     }
 }
