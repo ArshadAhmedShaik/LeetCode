030a9e637ad5e8791e4a3ffb204076d61ce8a2d7
Shaik Arshad Ahmed <shaik.arshad.dev@gmail.com>
2025-08-24 23:29:07 +0530

    Time: 3 ms (100%), Space: 57.8 MB (38.79%) - LeetHub

--- 2145-grid-game/2145-grid-game.java
+++ 2145-grid-game/2145-grid-game.java
@@ -63,25 +63,38 @@ class Solution {
 
         // return count;
 
+        // int n = grid[0].length;
+
+        // long[] preRow1 = new long[n];
+        // long[] preRow2 = new long[n];
+        // preRow1[0] = grid[0][0];
+        // preRow2[0] = grid[1][0];
+        // for (int i = 1; i < n; i++) {
+        //     preRow1[i] = preRow1[i - 1] + grid[0][i];
+        //     preRow2[i] = preRow2[i - 1] + grid[1][i];
+        // }
+
+        // long res = Long.MAX_VALUE;
+        // for (int i = 0; i < n; i++) {
+        //     long top = (i == n - 1 ? 0 : preRow1[n - 1] - preRow1[i]);
+        //     long bottom = (i == 0 ? 0 : preRow2[i - 1]);
+        //     long secondRobot = Math.max(top, bottom);
+        //     res = Math.min(res, secondRobot);
+        // }
+
+        // return res;
+
         int n = grid[0].length;
-
-        long[] preRow1 = new long[n];
-        long[] preRow2 = new long[n];
-        preRow1[0] = grid[0][0];
-        preRow2[0] = grid[1][0];
-        for (int i = 1; i < n; i++) {
-            preRow1[i] = preRow1[i - 1] + grid[0][i];
-            preRow2[i] = preRow2[i - 1] + grid[1][i];
-        }
-
+        long topSum = 0, bottomSum = 0;
+        for (int i = 0; i < n; i++)
+            topSum += grid[0][i];
         long res = Long.MAX_VALUE;
         for (int i = 0; i < n; i++) {
-            long top = (i == n - 1 ? 0 : preRow1[n - 1] - preRow1[i]);
-            long bottom = (i == 0 ? 0 : preRow2[i - 1]);
-            long secondRobot = Math.max(top, bottom);
+            topSum -= grid[0][i];
+            long secondRobot = Math.max(topSum, bottomSum);
             res = Math.min(res, secondRobot);
+            bottomSum += grid[1][i];
         }
-
         return res;
 
     }
