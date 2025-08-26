096faee48a12107177eb31d5d4d1537793fd3440
Shaik Arshad Ahmed <shaik.arshad.dev@gmail.com>
2025-08-26 21:42:18 +0530

    Time: 0 ms (100%), Space: 40.5 MB (92.22%) - LeetHub

--- 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.java
+++ 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.java
@@ -1,7 +1,9 @@
 class Solution {
     public int trailingZeroes(int n) {
+          
           int sum = 0;
           int i = 5;
+
           while(true) {
             int rem = n / i;
             i *= 5;
