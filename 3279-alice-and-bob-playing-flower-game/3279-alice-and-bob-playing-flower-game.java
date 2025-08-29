73a48132eda5183c12cf578c6429255cc43dad6f
Shaik Arshad Ahmed <shaik.arshad.dev@gmail.com>
2025-08-29 23:15:57 +0530

    Time: 0 ms (100%), Space: 41.2 MB (13.75%) - LeetHub

--- 3279-alice-and-bob-playing-flower-game/3279-alice-and-bob-playing-flower-game.java
+++ 3279-alice-and-bob-playing-flower-game/3279-alice-and-bob-playing-flower-game.java
@@ -1,13 +1,5 @@
-import java.math.*;
-
 class Solution {
     public long flowerGame(int n, int m) {
-        BigInteger N = BigInteger.valueOf(n);
-        BigInteger M = BigInteger.valueOf(m);
-        
-        BigInteger product = N.multiply(M);      
-        BigInteger result = product.divide(BigInteger.valueOf(2)); 
-        
-        return result.longValue(); 
+        return ( (long)((n + 1)/2) * (m/2) ) + ((long)(n/2) * ((m + 1)/2));
     }
 }
