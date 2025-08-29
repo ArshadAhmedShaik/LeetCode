db79ca62926ec512b737f94b2cc392ed243fd302
Shaik Arshad Ahmed <shaik.arshad.dev@gmail.com>
2025-08-30 01:36:25 +0530

    Time: 42 ms (31.8%), Space: 57 MB (86%) - LeetHub

--- 2448-count-number-of-bad-pairs/2448-count-number-of-bad-pairs.java
+++ 2448-count-number-of-bad-pairs/2448-count-number-of-bad-pairs.java
@@ -3,9 +3,8 @@ class Solution {
         long bad = 0;
         Map<Integer, Integer> map = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
-            int diff = i - nums[i];
-            bad += i - map.getOrDefault(diff, 0);
-            map.put(diff, map.getOrDefault(diff, 0) + 1);
+            bad += i - map.getOrDefault(i - nums[i], 0);
+            map.merge(i - nums[i], 1, Integer::sum);
         }
         return bad;
     }
