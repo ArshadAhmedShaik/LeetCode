e51f9cd57304b10bcafcec1c041c149408c3dad5
Shaik Arshad Ahmed <shaik.arshad.dev@gmail.com>
2025-08-29 23:16:52 +0530

    Time: 0 ms (100%), Space: 40.8 MB (40%) - LeetHub

--- 3279-alice-and-bob-playing-flower-game/3279-alice-and-bob-playing-flower-game.java
+++ 3279-alice-and-bob-playing-flower-game/3279-alice-and-bob-playing-flower-game.java
@@ -1,4 +1,6 @@
 class Solution {
+    // if x + y is odd then x -> even && y -> odd or else reverse
+    // only two possibilites are there
     public long flowerGame(int n, int m) {
         return ( (long)((n + 1)/2) * (m/2) ) + ((long)(n/2) * ((m + 1)/2));
     }
