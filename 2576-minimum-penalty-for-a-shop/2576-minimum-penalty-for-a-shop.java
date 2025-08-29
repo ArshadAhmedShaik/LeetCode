51943402686ba78dae45bec8410fe4f9db398d62
Shaik Arshad Ahmed <shaik.arshad.dev@gmail.com>
2025-08-29 10:49:05 +0530

    Time: 16 ms (7.87%), Space: 45.4 MB (50.8%) - LeetHub

--- 2576-minimum-penalty-for-a-shop/2576-minimum-penalty-for-a-shop.java
+++ 2576-minimum-penalty-for-a-shop/2576-minimum-penalty-for-a-shop.java
@@ -1,13 +1,37 @@
-public class Solution {
+class Solution {
     public int bestClosingTime(String customers) {
-        int max_score = 0, score = 0, best_hour = -1;
-        for(int i = 0; i < customers.length(); ++i) {
-            score += (customers.charAt(i) == 'Y') ? 1 : -1;
-            if(score > max_score) {
-                max_score = score;
-                best_hour = i;
+      int n = customers.length();
+      int[] left = new int[n];
+      int[] right = new int[n];
+      for(int i = 0;i < n;i++) {
+            if(customers.charAt(i)=='N') {
+                    left[i]= (i==0) ? 1 : left[i-1] + 1;
+            } else {
+                left[i] = (i==0) ? 0 : left[i-1];
+            } 
+      }
+      for(int i = n-1;i >= 0;i--) {
+            if(customers.charAt(i)=='Y') {
+                    right[i] = (i==n-1) ? 1 : right[i+1] + 1;
+            } else {
+                right[i] = (i==n-1) ? 0 : right[i+1];
             }
+      }
+
+        int minPenalty = Integer.MAX_VALUE;
+        int minHour = 0;
+
+        for(int i = 0;i <= n;i++) {
+                int leftPenalty = (i==0) ? 0 : left[i-1];
+                int rightPenalty = (i==n) ? 0 : right[i];
+                int totalPenalty = leftPenalty + rightPenalty;
+                if(totalPenalty < minPenalty) {
+                        minPenalty = totalPenalty;
+                        minHour = i;
+                }
         }
-        return best_hour + 1;
+
+        return minHour;
+
     }
 }
