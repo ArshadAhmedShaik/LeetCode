edef906d798c556869cd1be0c6ed26d6dc4b67b6
Shaik Arshad Ahmed <shaik.arshad.dev@gmail.com>
2025-08-30 01:35:29 +0530

    Time: 41 ms (39.54%), Space: 58.5 MB (48.11%) - LeetHub

--- 2448-count-number-of-bad-pairs/2448-count-number-of-bad-pairs.java
+++ 2448-count-number-of-bad-pairs/2448-count-number-of-bad-pairs.java
@@ -1,18 +1,12 @@
 class Solution {
     public long countBadPairs(int[] nums) {
-                     long badPairs = 0;
-    // map will just count the number of times good pairs are occuring to     the left of a particular element:
-                     Map<Integer, Integer> map = new HashMap<>();
-                     int n = nums.length;
-                     for(int i = 0;i < n;i++) {
-                            int diff = i - nums[i];
-                            int goodPairs = map.getOrDefault(diff, 0);
-
-                           badPairs += i - goodPairs;
-                           map.put(diff, map.getOrDefault(diff, 0)+1);         
-
-                     }
-
-             return badPairs;        
+        long bad = 0;
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int diff = i - nums[i];
+            bad += i - map.getOrDefault(diff, 0);
+            map.put(diff, map.getOrDefault(diff, 0) + 1);
+        }
+        return bad;
     }
 }
