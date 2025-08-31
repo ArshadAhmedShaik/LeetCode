e6cd8101dacd31eeb42d5f4c32cbb1a61da18ad7
Shaik Arshad Ahmed <shaik.arshad.dev@gmail.com>
2025-08-31 16:42:53 +0530

    Time: 6 ms (8.31%), Space: 55.6 MB (51.46%) - LeetHub

--- 2132-convert-1d-array-into-2d-array/2132-convert-1d-array-into-2d-array.java
+++ 2132-convert-1d-array-into-2d-array/2132-convert-1d-array-into-2d-array.java
@@ -4,14 +4,11 @@ class Solution {
 
             int[][] matrix = new int[m][n];
             if(len!=m*n) return new int[0][0]; // empty2d array
-            
-            int col = -1;
+
             for(int i = 0;i < len;i++) {
                     int row = i / n;
-                    if(i%n==0) {
-                        col = 0;
-                    }     
-                    matrix[row][col++] = original[i];
+                    int col = i % n;
+                    matrix[row][col] = original[i];
             }
 
 
