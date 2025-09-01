8500b8af916ff505f958737252f619e925973997
Shaik Arshad Ahmed <shaik.arshad.dev@gmail.com>
2025-09-02 00:28:54 +0530

    Time: 12 ms (26.15%), Space: 57 MB (68.7%) - LeetHub

--- 0878-shifting-letters/0878-shifting-letters.java
+++ 0878-shifting-letters/0878-shifting-letters.java
@@ -1,16 +1,11 @@
 class Solution {
     public String shiftingLetters(String s, int[] shifts) {
         StringBuilder sb = new StringBuilder();
-        int n = s.length();
-
-        long totalShift = 0;
-
-        for (int i = n - 1; i >= 0; i--) {
-            totalShift = (totalShift + shifts[i]) % 26;
-            int val = (s.charAt(i) - 'a' + (int) totalShift) % 26;
-            sb.append((char) ('a' + val));
+        long shift = 0;
+        for (int i = s.length() - 1; i >= 0; i--) {
+            shift = (shift + shifts[i]) % 26;
+            sb.append((char)('a' + (s.charAt(i) - 'a' + (int)shift) % 26));
         }
-
         return sb.reverse().toString();
     }
 }
