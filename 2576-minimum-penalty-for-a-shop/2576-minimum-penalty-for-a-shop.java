4420812f2058daa3db130c3e1ef0b1e7e89b55ea
Shaik Arshad Ahmed <shaik.arshad.dev@gmail.com>
2025-09-02 09:19:49 +0530

    Time: 11 ms (72.72%), Space: 45.5 MB (39.47%) - LeetHub

--- 2576-minimum-penalty-for-a-shop/2576-minimum-penalty-for-a-shop.java
+++ 2576-minimum-penalty-for-a-shop/2576-minimum-penalty-for-a-shop.java
@@ -1,35 +1,28 @@
 class Solution {
     public int bestClosingTime(String customers) {
         int n = customers.length();
-        int[] left = new int[n];
-        int[] right = new int[n];
+        int penalty = 0;
 
-         // left side -> "N"
+      
+        for (int i = 0; i < n; i++) {
+            if (customers.charAt(i) == 'Y') penalty++;
+        }
 
-         for(int i = 0;i < n;i++) {
-            if(customers.charAt(i) == 'N') left[i] = (i==0) ? 1 : left[i-1] + 1;
-            else left[i] = (i==0) ? 0 : left[i-1];
-         }
+        int minPenalty = penalty;
+        int minHour = 0;
 
-         // right side -> "Y"
+        for (int i = 0; i < n; i++) {
+            if (customers.charAt(i) == 'Y') {
+                penalty--; 
+            } else {
+                penalty++; 
+            }
+            if (penalty < minPenalty) {
+                minPenalty = penalty;
+                minHour = i + 1; 
+            }
+        }
 
-         for(int i = n - 1;i >= 0;i--) {
-                if(customers.charAt(i) == 'Y') right[i] = (i==n-1) ? 1 : right[i+1] + 1;
-                else right[i] = (i==n-1) ? 0 : right[i+1]; 
-         }
-
-        int minHours = -1;
-        int minPenalty = Integer.MAX_VALUE;
-
-         for(int i = 0;i <= n;i++) {
-                int leftPenalty = (i==0) ? 0 : left[i-1];
-                int rightPenalty = (i==n) ? 0 : right[i];
-                int totalPenalty = leftPenalty + rightPenalty;
-                if(totalPenalty<minPenalty) {
-                        minPenalty = totalPenalty;
-                        minHours = i;
-                }
-         }
-         return minHours;
+        return minHour;
     }
 }
