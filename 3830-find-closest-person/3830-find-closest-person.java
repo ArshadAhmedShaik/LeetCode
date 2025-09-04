10e856d8200e5dea00023b5007b542dcbe1268c3
Shaik Arshad Ahmed <shaik.arshad.dev@gmail.com>
2025-09-05 01:10:22 +0530

    Time: 1 ms (34.7%), Space: 40.7 MB (95.2%) - LeetHub

---
+++ 3830-find-closest-person/3830-find-closest-person.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int findClosest(int x, int y, int z) {
+        int firstDiff = Math.abs(x-z);
+        int secondDiff = Math.abs(y-z);
+
+        if(firstDiff < secondDiff) {
+                return 1;
+        } else if(firstDiff > secondDiff) {
+                return 2;
+        } else {
+            return 0;
+        }
+    }
+}
