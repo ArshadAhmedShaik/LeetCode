814030fd60849033ff083417087b12b5c4cdbb37
Shaik Arshad Ahmed <shaik.arshad.dev@gmail.com>
2025-09-06 00:04:10 +0530

    Time: 6 ms (13.15%), Space: 42.8 MB (17.98%) - LeetHub

--- 0067-add-binary/0067-add-binary.java
+++ 0067-add-binary/0067-add-binary.java
@@ -1,19 +1,14 @@
+import java.math.BigInteger;
+
 class Solution {
     public String addBinary(String a, String b) {
-        StringBuilder sb = new StringBuilder();
-        int i = a.length() - 1;
-        int j = b.length() - 1;
-        int carry = 0;
+       
+        BigInteger num1 = new BigInteger(a, 2);
+        BigInteger num2 = new BigInteger(b, 2);
 
-        while (i >= 0 || j >= 0 || carry > 0) {
-            int sum = carry;
-            if (i >= 0) sum += a.charAt(i--) - '0';
-            if (j >= 0) sum += b.charAt(j--) - '0';
+        
+        BigInteger sum = num1.add(num2);
 
-            sb.append(sum % 2);
-            carry = sum / 2;
-        }
-
-        return sb.reverse().toString();
+        return sum.toString(2);
     }
 }
