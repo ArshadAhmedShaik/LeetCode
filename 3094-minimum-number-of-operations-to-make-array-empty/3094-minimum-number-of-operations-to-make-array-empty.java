2d4b1125145b48bb45504e0a29a4c0a420b8dfa8
Shaik Arshad Ahmed <shaik.arshad.dev@gmail.com>
2025-09-08 00:10:11 +0530

    Time: 21 ms (51%), Space: 63 MB (37.44%) - LeetHub

--- 3094-minimum-number-of-operations-to-make-array-empty/3094-minimum-number-of-operations-to-make-array-empty.java
+++ 3094-minimum-number-of-operations-to-make-array-empty/3094-minimum-number-of-operations-to-make-array-empty.java
@@ -1,29 +1,18 @@
-class Solution {
+public class Solution {
     public int minOperations(int[] nums) {
-        Arrays.sort(nums);
-        int n = nums.length;
-        int ans = 0;
-        int i = 0;
-
-        while (i < n) {
-            int count = 1;
-   
-            while (i + count < n && nums[i + count] == nums[i]) {
-                count++;
-            }
-
-            if (count == 1) return -1;
-
-            if (count % 3 == 0) {
-                ans += count / 3;
-            } else if (count % 3 == 1) {
-                ans += (count - 4) / 3 + 2; 
-            } else { 
-                ans += count / 3 + 1;
-            }
-
-            i += count; 
+        Map<Integer, Integer> count = new HashMap<>();
+        for (int num : nums) {
+            count.put(num, count.getOrDefault(num, 0) + 1);
         }
-        return ans;
+
+        int res = 0;
+        for (int cnt : count.values()) {
+            if (cnt == 1) {
+                return -1;
+            }
+            res += (cnt + 2) / 3;
+        }
+
+        return res;
     }
 }
