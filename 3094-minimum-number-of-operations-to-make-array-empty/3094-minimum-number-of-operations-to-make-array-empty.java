275bb1c19423fd46091cbaf971b112bad01e35e7
Shaik Arshad Ahmed <shaik.arshad.dev@gmail.com>
2025-09-08 00:06:05 +0530

    Time: 5 ms (100%), Space: 58.3 MB (85.68%) - LeetHub

--- 3094-minimum-number-of-operations-to-make-array-empty/3094-minimum-number-of-operations-to-make-array-empty.java
+++ 3094-minimum-number-of-operations-to-make-array-empty/3094-minimum-number-of-operations-to-make-array-empty.java
@@ -1,28 +1,29 @@
-import java.util.HashMap;
-import java.util.Map;
-
 class Solution {
     public int minOperations(int[] nums) {
+        Arrays.sort(nums);
+        int n = nums.length;
+        int ans = 0;
+        int i = 0;
 
-        Map<Integer, Integer> freq = new HashMap<>();
-        for (int x : nums) {
-            freq.put(x, freq.getOrDefault(x, 0) + 1);
-        }
-
-        int operations = 0;
-
-        for (int count : freq.values()) {
-            if (count == 1) return -1; 
-            else if (count % 3 == 0) {
-                operations += count / 3;
-            } else if (count % 3 == 1) {
-                if (count < 4) return -1; 
-                operations += (count - 4) / 3 + 2;
-            } else { 
-                operations += (count - 2) / 3 + 1; 
+        while (i < n) {
+            int count = 1;
+   
+            while (i + count < n && nums[i + count] == nums[i]) {
+                count++;
             }
-        }
 
-        return operations;
+            if (count == 1) return -1;
+
+            if (count % 3 == 0) {
+                ans += count / 3;
+            } else if (count % 3 == 1) {
+                ans += (count - 4) / 3 + 2; 
+            } else { 
+                ans += count / 3 + 1;
+            }
+
+            i += count; 
+        }
+        return ans;
     }
 }
