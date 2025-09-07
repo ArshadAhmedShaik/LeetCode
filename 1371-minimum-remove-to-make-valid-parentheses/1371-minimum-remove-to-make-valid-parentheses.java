5d81aa42c4b4cfe628c3024c0a887c738b6a3059
Shaik Arshad Ahmed <shaik.arshad.dev@gmail.com>
2025-09-07 23:09:37 +0530

    Time: 16 ms (79.79%), Space: 45.7 MB (45.48%) - LeetHub

--- 1371-minimum-remove-to-make-valid-parentheses/1371-minimum-remove-to-make-valid-parentheses.java
+++ 1371-minimum-remove-to-make-valid-parentheses/1371-minimum-remove-to-make-valid-parentheses.java
@@ -1,25 +1,29 @@
 class Solution {
     public String minRemoveToMakeValid(String s) {
-           StringBuilder sb = new StringBuilder();
            int n = s.length();
+           char[] sb = new char[n];
            int count = 0;
+           int index = 0;
            for(int i = 0;i < n;i++) {
                 char ch = s.charAt(i);
-                if(Character.isLetter(ch)) sb.append(ch);
+                if(Character.isLetter(ch)) {
+                        sb[index++] = ch;   
+                }
                 else if(ch == '(') {
                         count++;
-                        sb.append(ch);
+                        sb[index++] = ch;
                 } else if(count > 0 && ch == ')') {
-                        sb.append(ch);
+                        sb[index++] = ch;
                         count--;
                 }
            }
+           char[] ans1 = Arrays.copyOf(sb, index);
            StringBuilder ans = new StringBuilder();
-           for(int i = sb.length()-1;i>=0;i--) {
-                if(sb.charAt(i) == '(' && count > 0) {
+           for(int i = ans1.length - 1;i>=0;i--) {
+                if(sb[i] == '(' && count > 0) {
                         count--;
                 } else {
-                        ans.append(sb.charAt(i));
+                        ans.append(sb[i]);
                 }
            }
         return ans.reverse().toString();
