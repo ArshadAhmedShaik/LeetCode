30b97e53d0a11fc010c90b23decdf535bd1b38b5
Shaik Arshad Ahmed <shaik.arshad.dev@gmail.com>
2025-09-07 23:11:00 +0530

    Time: 12 ms (87.11%), Space: 45.4 MB (87.05%) - LeetHub

--- 1371-minimum-remove-to-make-valid-parentheses/1371-minimum-remove-to-make-valid-parentheses.java
+++ 1371-minimum-remove-to-make-valid-parentheses/1371-minimum-remove-to-make-valid-parentheses.java
@@ -1,31 +1,28 @@
 class Solution {
     public String minRemoveToMakeValid(String s) {
-           int n = s.length();
-           char[] sb = new char[n];
-           int count = 0;
-           int index = 0;
-           for(int i = 0;i < n;i++) {
-                char ch = s.charAt(i);
-                if(Character.isLetter(ch)) {
-                        sb[index++] = ch;   
-                }
-                else if(ch == '(') {
-                        count++;
-                        sb[index++] = ch;
-                } else if(count > 0 && ch == ')') {
-                        sb[index++] = ch;
-                        count--;
-                }
-           }
-           char[] ans1 = Arrays.copyOf(sb, index);
-           StringBuilder ans = new StringBuilder();
-           for(int i = ans1.length - 1;i>=0;i--) {
-                if(sb[i] == '(' && count > 0) {
-                        count--;
+        int n = s.length();
+        char[] arr = s.toCharArray();
+        int count = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (arr[i] == '(') {
+                count++;
+            } else if (arr[i] == ')') {
+                if (count == 0) {
+                    arr[i] = 0; 
                 } else {
-                        ans.append(sb[i]);
+                    count--;
                 }
-           }
-        return ans.reverse().toString();
-    }    
+            }
+        }
+
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = n - 1; i >= 0; i--) {
+            if (arr[i] == '(' && count-- > 0) continue; 
+            if (arr[i] != 0) sb.append(arr[i]);
+        }
+
+        return sb.reverse().toString();
+    }
 }
