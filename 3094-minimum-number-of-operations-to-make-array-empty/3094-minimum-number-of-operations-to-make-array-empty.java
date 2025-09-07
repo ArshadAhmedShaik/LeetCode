694b9bad58888f652e33897efc4f93f6fee17cc7
Shaik Arshad Ahmed <shaik.arshad.dev@gmail.com>
2025-09-08 00:05:09 +0530

    Time: 23 ms (25.63%), Space: 63.2 MB (18.59%) - LeetHub

--- 3094-minimum-number-of-operations-to-make-array-empty/3094-minimum-number-of-operations-to-make-array-empty.java
+++ 3094-minimum-number-of-operations-to-make-array-empty/3094-minimum-number-of-operations-to-make-array-empty.java
@@ -3,42 +3,26 @@ import java.util.Map;
 
 class Solution {
     public int minOperations(int[] nums) {
-        if (nums == null || nums.length == 0) return 0;
 
-        int MAX_ARRAY = 1_000_000;
-        int[] counts = new int[MAX_ARRAY + 1];
-        Map<Integer, Integer> largeCounts = new HashMap<>();
-
+        Map<Integer, Integer> freq = new HashMap<>();
         for (int x : nums) {
-            if (x <= MAX_ARRAY) counts[x]++;
-            else largeCounts.put(x, largeCounts.getOrDefault(x, 0) + 1);
+            freq.put(x, freq.getOrDefault(x, 0) + 1);
         }
 
         int operations = 0;
 
-        for (int count : counts) {
-            if (count == 0) continue;
-            int ops = minOps(count);
-            if (ops == -1) return -1;
-            operations += ops;
-        }
-
-        for (int count : largeCounts.values()) {
-            int ops = minOps(count);
-            if (ops == -1) return -1;
-            operations += ops;
+        for (int count : freq.values()) {
+            if (count == 1) return -1; 
+            else if (count % 3 == 0) {
+                operations += count / 3;
+            } else if (count % 3 == 1) {
+                if (count < 4) return -1; 
+                operations += (count - 4) / 3 + 2;
+            } else { 
+                operations += (count - 2) / 3 + 1; 
+            }
         }
 
         return operations;
-    }
-
-    private int minOps(int count) {
-        if (count == 1) return -1;
-        if (count % 3 == 0) return count / 3;
-        if (count % 3 == 1) {
-            if (count < 4) return -1;
-            return (count - 4) / 3 + 2;
-        }
-        return (count - 2) / 3 + 1;
     }
 }
