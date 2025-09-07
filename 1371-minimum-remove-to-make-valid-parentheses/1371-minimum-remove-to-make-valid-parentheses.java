c744049540649520d2111d533c1c7c81eea13eff
Shaik Arshad Ahmed <shaik.arshad.dev@gmail.com>
2025-09-07 23:11:53 +0530

    Time: 5 ms (99.92%), Space: 45.5 MB (61.69%) - LeetHub

--- 1371-minimum-remove-to-make-valid-parentheses/1371-minimum-remove-to-make-valid-parentheses.java
+++ 1371-minimum-remove-to-make-valid-parentheses/1371-minimum-remove-to-make-valid-parentheses.java
@@ -1,7 +1,7 @@
 class Solution {
     public String minRemoveToMakeValid(String s) {
-        int n = s.length();
         char[] arr = s.toCharArray();
+        int n = arr.length;
         int count = 0;
 
         for (int i = 0; i < n; i++) {
@@ -9,20 +9,26 @@ class Solution {
                 count++;
             } else if (arr[i] == ')') {
                 if (count == 0) {
-                    arr[i] = 0; 
+                    arr[i] = 0;
                 } else {
                     count--;
                 }
             }
         }
 
-
-        StringBuilder sb = new StringBuilder();
-        for (int i = n - 1; i >= 0; i--) {
-            if (arr[i] == '(' && count-- > 0) continue; 
-            if (arr[i] != 0) sb.append(arr[i]);
+        for (int i = n - 1; i >= 0 && count > 0; i--) {
+            if (arr[i] == '(') {
+                arr[i] = 0;
+                count--;
+            }
         }
 
-        return sb.reverse().toString();
+        char[] result = new char[n];
+        int idx = 0;
+        for (char c : arr) {
+            if (c != 0) result[idx++] = c;
+        }
+
+        return new String(result, 0, idx);
     }
 }
