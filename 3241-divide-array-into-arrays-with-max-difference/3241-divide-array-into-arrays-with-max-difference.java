290e55d21a1b5d50821c2d0b92de7dee37283736
Shaik Arshad Ahmed <shaik.arshad.dev@gmail.com>
2025-09-10 01:03:57 +0530

    Time: 6 ms (100%), Space: 56.8 MB (95.78%) - LeetHub

--- 3241-divide-array-into-arrays-with-max-difference/3241-divide-array-into-arrays-with-max-difference.java
+++ 3241-divide-array-into-arrays-with-max-difference/3241-divide-array-into-arrays-with-max-difference.java
@@ -1,13 +1,29 @@
 class Solution {
     public int[][] divideArray(int[] nums, int k) {
-        Arrays.sort(nums);
-        int[][] ans = new int[nums.length / 3][3];
-        for (int i = 0; i < nums.length; i += 3) {
-            if (nums[i + 2] - nums[i] > k) {
-                return new int[0][0];
+        int max = 0;
+        for (int i = 0 ; i < nums.length; i++) {
+            if (max < nums[i]) max = nums[i];
+        } 
+        int[] sort = new int[max+1];
+        for (int i = 0 ; i < nums.length; i++) {
+            sort[nums[i]]++;
+        } 
+        int p = 0;
+        for (int i = 0; i < sort.length; i++) {
+            int c = sort[i];
+            for (int j = 0; j < c; j++) {
+                nums[p++] = i;
             }
-            ans[i / 3] = new int[]{nums[i], nums[i + 1], nums[i + 2]};
         }
-        return ans;
+        
+        for (int i = 0; i < nums.length; i+=3) {
+            if (nums[i + 2] - nums[i] > k) return new int[0][];
+        }
+        int[][] result = new int[nums.length / 3][];
+        p = 0;
+        for (int i = 0; i < result.length; i ++) {
+            result[i] = new int[] {nums[p++], nums[p++], nums[p++]};
+        }
+        return result;
     }
 }
