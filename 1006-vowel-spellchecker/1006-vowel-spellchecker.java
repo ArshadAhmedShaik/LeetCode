af3e31bac6758ada1a7b0bf8e5d00ec122dd453d
Shaik Arshad Ahmed <shaik.arshad.dev@gmail.com>
2025-09-15 01:15:54 +0530

    Time: 22 ms (75.6%), Space: 48.2 MB (63.16%) - LeetHub

--- 1006-vowel-spellchecker/1006-vowel-spellchecker.java
+++ 1006-vowel-spellchecker/1006-vowel-spellchecker.java
@@ -1,51 +1,54 @@
 class Solution {
     public String[] spellchecker(String[] wordlist, String[] queries) {
-        int m = wordlist.length, n = queries.length;
-        String[] res = new String[n];
-        Map<String, Integer> caseSense = new HashMap<>();
-        Map<String, Integer> caseInsense = new HashMap<>();
-        Map<String, Integer> vowelErrors = new HashMap<>();
-        for (int i = m - 1; i >= 0; i--) {
-            String word = wordlist[i];
-            caseSense.put(word, i);
-            String lowerCase = word.toLowerCase();
-            char[] vowelRepArr = lowerCase.toCharArray();
-            for (int j = 0; j < vowelRepArr.length; j++) {
-                if (vowelRepArr[j] == 'e' || vowelRepArr[j] == 'i' || vowelRepArr[j] == 'o' || vowelRepArr[j] == 'u') {
-                    vowelRepArr[j] = 'a';
-                }
-            }
-            caseInsense.put(lowerCase, i);
-            vowelErrors.put(new String(vowelRepArr), i);
-        }
-        for (int i = 0; i < n; i++) {
-            String word = queries[i];
-            if (caseSense.containsKey(word)) {
-                res[i] = word;
-                continue;
-            }
+        Set<String> exactSet = new HashSet<>();
+        Map<String, String> lowerMap = new HashMap<>();
+        Map<String, String> vowelMap = new HashMap<>();
 
-            String lowerCase = word.toLowerCase();
-            if (caseInsense.containsKey(lowerCase)) {
-                res[i] = wordlist[caseInsense.get(lowerCase)];
-                continue;
-            }
+        Set<Character> vowelSet = new HashSet<>(Arrays.asList('a','e','i','o','u'));
 
-            char[] vowelRepArr = lowerCase.toCharArray();
-            for (int j = 0; j < vowelRepArr.length; j++) {
-                if (vowelRepArr[j] == 'e' || vowelRepArr[j] == 'i' || vowelRepArr[j] == 'o' || vowelRepArr[j] == 'u') {
-                    vowelRepArr[j] = 'a';
-                }
-            }
-            String vowelRepStr = new String(vowelRepArr);
-            if (vowelErrors.containsKey(vowelRepStr)) {
-                res[i] = wordlist[vowelErrors.get(vowelRepStr)];
-                continue;
-            }
-            res[i] = "";
+// we care only abt first occurence: 
+
+        for (String word : wordlist) {
+            exactSet.add(word);
+            String lower = word.toLowerCase();
+            lowerMap.putIfAbsent(lower, word);
+            String vowelKey = devowel(lower, vowelSet);
+            vowelMap.putIfAbsent(vowelKey, word);
         }
 
-        return res;
+        String[] result = new String[queries.length];
 
+        for (int i = 0; i < queries.length; i++) {
+            String query = queries[i];
+            if (exactSet.contains(query)) {
+                result[i] = query;
+            } else {
+                String lower = query.toLowerCase();
+                if (lowerMap.containsKey(lower)) {
+                    result[i] = lowerMap.get(lower);
+                } else {
+                    String vowelKey = devowel(lower, vowelSet);
+                    if (vowelMap.containsKey(vowelKey)) {
+                        result[i] = vowelMap.get(vowelKey);
+                    } else {
+                        result[i] = "";
+                    }
+                }
+            }
+        }
+        return result;
+    }
+
+    private String devowel(String word, Set<Character> vowelSet) {
+        char[] sb = new char[word.length()];
+        int index = 0;
+        for (char c : word.toCharArray()) {
+            if (vowelSet.contains(c)) {
+                sb[index++] = '*';
+            } else {
+                sb[index++] = c;
+            }
+        }
+        return new String(sb);
     }
 }
