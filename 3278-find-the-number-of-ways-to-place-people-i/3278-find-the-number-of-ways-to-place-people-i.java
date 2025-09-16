34aeb050c13f5d468e0c1c6264d59492a03ecf8e
Shaik Arshad Ahmed <shaik.arshad.dev@gmail.com>
2025-09-16 20:24:29 +0530

    Time: 5 ms (96.41%), Space: 44.7 MB (41.93%) - LeetHub

--- 3278-find-the-number-of-ways-to-place-people-i/3278-find-the-number-of-ways-to-place-people-i.java
+++ 3278-find-the-number-of-ways-to-place-people-i/3278-find-the-number-of-ways-to-place-people-i.java
@@ -1,33 +1,30 @@
 class Solution {
     public int numberOfPairs(int[][] points) {
-       Arrays.sort(points, (a, b) -> {
-             if(a[0]!=b[0]) return a[0] - b[0];
-             else {
-                return b[1] - a[1];
-             }
-       });
-      int n = points.length;  
-      int count = 0;
-      for(int i = 0;i < n;i++) {
+     Arrays.sort(points, (a, b)-> {
+         if(a[0]!=b[0]) return a[0] - b[0];
+         else return b[1] - a[1];
+     });
+     int n = points.length;
+     int count = 0;
+     for(int i = 0;i < n;i++) {
             int x1 = points[i][0];
             int y1 = points[i][1];
-            int maxY = Integer.MIN_VALUE;
+            int max = Integer.MIN_VALUE;
             for(int j = i + 1;j < n;j++) {
-
                     int x2 = points[j][0];
                     int y2 = points[j][1];
                     if(y2>y1) {
                         continue;
                     } else {
-                        if(maxY<y2) {
-                                count++;
+                        if(max<y2) {
+                            count++;
                         }
                     }
-                    maxY = Math.max(maxY, y2);    
+
+            max = Math.max(max, y2);
+
             }
-      }
-
-      return count;
-
+     }
+     return count;
     }
 }
