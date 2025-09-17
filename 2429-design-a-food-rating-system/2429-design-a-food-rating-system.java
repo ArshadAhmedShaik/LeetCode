90bde74e6e58a962826340a1c5fab93866dc9173
Shaik Arshad Ahmed <shaik.arshad.dev@gmail.com>
2025-09-17 22:18:18 +0530

    Time: 317 ms (13.03%), Space: 73.5 MB (72.42%) - LeetHub

--- 2429-design-a-food-rating-system/2429-design-a-food-rating-system.java
+++ 2429-design-a-food-rating-system/2429-design-a-food-rating-system.java
@@ -1,43 +1,41 @@
-public class FoodRatings {
-    private Map<String, Integer> foodToRating;
-    private Map<String, String> foodToCuisine;
-    private Map<String, TreeSet<FoodPair>> cuisineToSortedSet;
-
-    private static class FoodPair {
-        int rating;
-        String food;
-
-        FoodPair(int rating, String food) {
-            this.rating = rating;
-            this.food = food;
-        }
-    }
+class FoodRatings {
+    Map<String, Integer> foodToRatings = new HashMap<>();
+    Map<String, String> foodToCuisine = new HashMap<>();
+    Map<String, TreeSet<String>> cuisineToFoods = new HashMap<>();
 
     public FoodRatings(String[] foods, String[] cuisines, int[] ratings) {
-        foodToRating = new HashMap<>();
-        foodToCuisine = new HashMap<>();
-        cuisineToSortedSet = new HashMap<>();
+        int n = foods.length;
+        for (int i = 0; i < n; i++) {
+            String food = foods[i];
+            String cuisine = cuisines[i];
+            int rating = ratings[i];
 
-        for (int i = 0; i < foods.length; i++) {
-            foodToRating.put(foods[i], ratings[i]);
-            foodToCuisine.put(foods[i], cuisines[i]);
-            cuisineToSortedSet.computeIfAbsent(cuisines[i], k -> new TreeSet<>((a, b) -> {
-                if (a.rating != b.rating) return b.rating - a.rating;
-                return a.food.compareTo(b.food);
-            })).add(new FoodPair(ratings[i], foods[i]));
+            foodToRatings.put(food, rating);
+            foodToCuisine.put(food, cuisine);
+
+            cuisineToFoods.putIfAbsent(cuisine,
+                new TreeSet<>((a, b) -> {
+                    int cmp = Integer.compare(foodToRatings.get(b), foodToRatings.get(a)); 
+                    if (cmp == 0) return a.compareTo(b); 
+                    return cmp;
+                })
+            );
+
+            cuisineToFoods.get(cuisine).add(food);
         }
     }
 
     public void changeRating(String food, int newRating) {
         String cuisine = foodToCuisine.get(food);
-        int oldRating = foodToRating.get(food);
-        TreeSet<FoodPair> set = cuisineToSortedSet.get(cuisine);
-        set.remove(new FoodPair(oldRating, food));
-        foodToRating.put(food, newRating);
-        set.add(new FoodPair(newRating, food));
+        TreeSet<String> set = cuisineToFoods.get(cuisine);
+
+      
+        set.remove(food);
+        foodToRatings.put(food, newRating);
+        set.add(food);
     }
 
     public String highestRated(String cuisine) {
-        return cuisineToSortedSet.get(cuisine).first().food;
+        return cuisineToFoods.get(cuisine).first();
     }
 }
