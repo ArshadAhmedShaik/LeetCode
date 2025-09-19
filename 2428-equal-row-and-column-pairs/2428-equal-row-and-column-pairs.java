0b9e89ccf67dc32d0f7aa0121c7f6e2588f9aeef
Shaik Arshad Ahmed <shaik.arshad.dev@gmail.com>
2025-09-19 22:14:30 +0530

    Time: 18 ms (89.76%), Space: 50.3 MB (53.11%) - LeetHub

--- 2428-equal-row-and-column-pairs/2428-equal-row-and-column-pairs.java
+++ 2428-equal-row-and-column-pairs/2428-equal-row-and-column-pairs.java
@@ -1,24 +1,29 @@
+import java.util.*;
+
 class Solution {
     public int equalPairs(int[][] grid) {
-   
-        Map<String, Integer> rowMap = new HashMap<>();
         int n = grid.length;
-        for(int i = 0;i < n;i++) {
-            StringBuilder sb = new StringBuilder();
-            for(int j = 0;j < n;j++) {
-                sb.append(grid[i][j]+".");
+        Map<List<Integer>, Integer> rowMap = new HashMap<>();
+
+
+        for (int i = 0; i < n; i++) {
+            List<Integer> row = new ArrayList<>();
+            for (int j = 0; j < n; j++) {
+                row.add(grid[i][j]);
             }
-            rowMap.put(sb.toString(), rowMap.getOrDefault(sb.toString(), 0)+1);
+            rowMap.put(row, rowMap.getOrDefault(row, 0) + 1);
         }
 
         int count = 0;
-        for(int i = 0;i < n;i++) {
-            StringBuilder sb = new StringBuilder();
-            for(int j = 0;j < n;j++) {
-                    sb.append(grid[j][i]+".");
+    
+        for (int i = 0; i < n; i++) {
+            List<Integer> col = new ArrayList<>();
+            for (int j = 0; j < n; j++) {
+                col.add(grid[j][i]);
             }
-            count += rowMap.getOrDefault(sb.toString(), 0);
+            count += rowMap.getOrDefault(col, 0);
         }
+
         return count;
     }
 }
