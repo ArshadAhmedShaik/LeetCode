a96d6bb1eb77b8071b35513584473291b8143ac3
Shaik Arshad Ahmed <shaik.arshad.dev@gmail.com>
2025-09-23 14:04:03 +0530

    Time: 28 ms (18.81%), Space: 45.4 MB (25.19%) - LeetHub

--- 2486-most-frequent-even-element/2486-most-frequent-even-element.java
+++ 2486-most-frequent-even-element/2486-most-frequent-even-element.java
@@ -1,18 +1,21 @@
 class Solution {
     public int mostFrequentEven(int[] nums) {
-        int[] count = new int[100001];
+        Map<Integer, Integer> map = new HashMap<>();
         for(int i = 0;i < nums.length;i++) {
-              count[nums[i]]++;
+              map.put(nums[i], map.getOrDefault(nums[i], 0)+1);
         }
         int maxFreq = 0;
         int element = -1;
-        for(int i = 0;i < count.length;i++) {
-            if(i%2==0) {
-                if(count[i] > maxFreq) {
-                    maxFreq = count[i];
-                    element = i;
+        for(var x: map.entrySet()) {
+                int key = x.getKey();
+                int value = x.getValue();
+                if(key%2!=0) continue;
+                if(value > maxFreq) {
+                    maxFreq = value;
+                    element = key;
+                } else if(value == maxFreq) {
+                    element = Math.min(element, key);
                 }
-            }
         }
         return element;
     }
