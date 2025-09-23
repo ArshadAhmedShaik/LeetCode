4742311603744a09311cea4d4dc4474d1ea876a2
Shaik Arshad Ahmed <shaik.arshad.dev@gmail.com>
2025-09-23 14:19:52 +0530

    Time: 3 ms (60.76%), Space: 17.7 MB (96.31%) - LeetHub

--- 1741-sort-array-by-increasing-frequency/1741-sort-array-by-increasing-frequency.py
+++ 1741-sort-array-by-increasing-frequency/1741-sort-array-by-increasing-frequency.py
@@ -1,11 +1,4 @@
-from functools import cmp_to_key
-
 class Solution:
     def frequencySort(self, nums: List[int]) -> List[int]:
         count = Counter(nums)
-        def func(x1, x2):
-             if(count[x1]==count[x2]):
-                return x2 - x1 
-             return count[x1] - count[x2]
-        sort = sorted(nums, key = cmp_to_key(func))
-        return sort
+        return sorted(nums, key=lambda x: (count[x], -x))
